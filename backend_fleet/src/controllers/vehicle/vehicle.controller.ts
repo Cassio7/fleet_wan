061faea259d8034e6ad73c7e9cf9ecdc504a1306
AlessioC <AlessioC@nomail.com>
2024-10-15 14:29:25 +0000

    get control

--- backend_fleet/src/controllers/vehicle/vehicle.controller.ts
+++ backend_fleet/src/controllers/vehicle/vehicle.controller.ts
@@ -5,11 +5,54 @@ import { VehicleService } from 'src/services/vehicle/vehicle.service';
 @Controller('vehicles')
 export class VehicleController {
   constructor(private readonly vehicleService: VehicleService) {}
+
+  @Get()
+  async getAllVehicles(@Res() res: Response) {
+    try {
+      const groups = await this.vehicleService.getAllVehicles();
+      res.status(200).json(groups);
+    } catch (error) {
+      console.error('Errore nel recupero dei veicoli:', error);
+      res.status(500).send('Errore durante il recupero dei veicoli');
+    }
+  }
+
+  @Get('/reader')
+  async getVehiclesByReader(@Res() res: Response) {
+    try {
+      const groups = await this.vehicleService.getVehiclesByReader();
+      res.status(200).json(groups);
+    } catch (error) {
+      console.error('Errore nel recupero dei veicoli:', error);
+      res.status(500).send('Errore durante il recupero dei veicoli');
+    }
+  }
+
+  @Get('/:id')
+  async getVehicleById(@Res() res: Response, @Param() params: any) {
+    try {
+      const group = await this.vehicleService.getVehicleById(params.id);
+      res.status(200).json(group);
+    } catch (error) {
+      console.error('Errore nel recupero dei veicoli:', error);
+      res.status(500).send('Errore durante il recupero dei veicoli');
+    }
+  }
+
   @Get('update/:id')
   async getVehicleList(@Res() res: Response, @Param() params: any) {
     try {
       const data = await this.vehicleService.getVehicleList(params.id);
-      res.status(200).json(data);
+
+      if (data.length > 0) {
+        let resultMessage: string = `Gruppo di aggiornamento id : ${params.id}\n\n`;
+        for (const item of data) {
+          resultMessage += `Aggiornati Veicolo id: ${item.veId}\n `;
+        }
+        res.status(200).send(resultMessage);
+      } else {
+        res.status(200).send('Nessun aggiornamento');
+      }
     } catch (error) {
       console.error('Errore nella richiesta SOAP:', error);
       res.status(500).send('Errore durante la richiesta al servizio SOAP');
