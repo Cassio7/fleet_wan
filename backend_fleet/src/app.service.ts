8a6682d7970e64edbbee7b3b11e2543362473266
AlessioC <AlessioC@nomail.com>
2024-10-16 15:20:31 +0000

    onInit seeding db

--- backend_fleet/src/app.service.ts
+++ backend_fleet/src/app.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { GroupService } from './services/group/group.service';
+import { VehicleService } from './services/vehicle/vehicle.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(
+    private readonly groupService: GroupService,
+    private readonly vehicleService: VehicleService,
+  ) {}
+  
+  // popolo database all'avvio
+  async onModuleInit() {
+    await this.groupService.getGroupList();
+    await this.vehicleService.getVehicleList(313);
+  }
   getHome(): string {
     return 'Fleet App';
   }
