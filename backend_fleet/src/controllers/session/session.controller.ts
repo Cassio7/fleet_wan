51d9c392cae3ebe7e7d03cfc86c86c60ff62cd14
AlessioC <AlessioC@nomail.com>
2024-10-22 11:52:09 +0000

    handle session service

--- backend_fleet/src/controllers/session/session.controller.ts
+++ backend_fleet/src/controllers/session/session.controller.ts
@@ -1,10 +1,58 @@
-import { Controller, Post, Res, Param, Body } from '@nestjs/common';
+import { Controller, Post, Res, Param, Body, Get } from '@nestjs/common';
 import { Response } from 'express';
-import { HistoryService } from 'src/services/session/session.service';
+import { SessionService } from 'src/services/session/session.service';
 
 @Controller('session')
-export class HistoryController {
-  constructor(private readonly historyService: HistoryService) {}
+export class SessionController {
+  constructor(private readonly sessionService: SessionService) {}
+
+  @Get(':id')
+  async getAllSessionByVeId(@Res() res: Response, @Param() params: any) {
+    const data = await this.sessionService.getAllSessionByVeId(params.id);
+    if (data.length > 0) {
+      res.status(200).send(data);
+    } else res.status(200).send(`No Session per id: ${params.id}`);
+  }
+
+  @Post('ranged/:id')
+  async getAllSessionByVeIdRanged(
+    @Res() res: Response,
+    @Param() params: any,
+    @Body() body: any,
+  ) {
+    const dateFrom = body.dateFrom;
+    const dateTo = body.dateTo;
+    // Controlla se dateFrom e dateTo sono forniti
+    if (!dateFrom || !dateTo) {
+      return res.status(400).send('Date non fornite.');
+    }
+
+    // Crea un oggetto Date dalla stringa fornita
+    const dateFrom_new = new Date(dateFrom);
+    const dateTo_new = new Date(dateTo);
+
+    // Controlla se la data è valida
+    if (isNaN(dateFrom_new.getTime()) || isNaN(dateTo_new.getTime())) {
+      return res.status(400).send('Formato della data non valido.');
+    }
+    if (dateFrom_new.getTime() >= dateTo_new.getTime()) {
+      // Restituisci un errore se la condizione è vera
+      return res
+        .status(400)
+        .send(
+          'La data iniziale deve essere indietro di almeno 1 giorno dalla finale',
+        );
+    }
+    const data = await this.sessionService.getAllSessionByVeIdRanged(
+      params.id,
+      dateFrom_new,
+      dateTo_new,
+    );
+    if (data.length > 0) {
+      res.status(200).send(data);
+    } else res.status(200).send(`No Session per id: ${params.id}`);
+  }
+
   @Post('update/:id')
   async getHistoryList(
     @Res() res: Response,
@@ -35,7 +83,7 @@ export class HistoryController {
             'La data iniziale deve essere indietro di almeno 1 giorno dalla finale',
           );
       }
-      const data = await this.historyService.getHistoryList(
+      const data = await this.sessionService.getSessionist(
         params.id,
         dateFrom_new.toISOString(),
         dateTo_new.toISOString(),
