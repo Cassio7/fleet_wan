e3f5cd74fabd9b8649fc97e2fb0f60d8e0a7975a
AlessioC <AlessioC@nomail.com>
2024-10-23 14:15:26 +0000

    fix for

--- backend_fleet/src/app.service.ts
+++ backend_fleet/src/app.service.ts
@@ -12,7 +12,7 @@ export class AppService implements OnModuleInit {
 
   // popolo database all'avvio
   async onModuleInit() {
-    //await this.putDbData();
+    await this.putDbData();
   }
   getHome(): string {
     return 'Fleet App';
@@ -20,19 +20,20 @@ export class AppService implements OnModuleInit {
 
   async putDbData() {
     await this.groupService.getGroupList();
+    // const groups = await this.groupService.getAllGroups()
+    // groups.reverse();
+    // for (const group of groups){
+    //   await this.vehicleService.getVehicleList(group.vgId);
+    // }
     await this.vehicleService.getVehicleList(313);
     const vehicles = await this.vehicleService.getAllVehicles();
     for (const vehicle of vehicles) {
-      console.log(vehicle.veId);
+      console.log(`${vehicle.veId} - ${vehicle.id}`);
       await this.sessionService.getSessionist(
         vehicle.veId,
-        '2024-10-20T00:00:00.000Z',
         '2024-10-21T00:00:00.000Z',
+        '2024-10-23T00:00:00.000Z',
       );
-      await this.sleep(1000);
     }
-  }
-  async sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
