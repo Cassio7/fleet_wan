c1b67cf2ac2e36a6f5c89ab9090fa602674cce7c
AlessioC <AlessioC@nomail.com>
2024-10-29 12:44:43 +0000

    tag oninit

--- backend_fleet/src/app.service.ts
+++ backend_fleet/src/app.service.ts
@@ -3,12 +3,14 @@ import { GroupService } from './services/group/group.service';
 import { VehicleService } from './services/vehicle/vehicle.service';
 import { SessionService } from './services/session/session.service';
 import { Cron } from '@nestjs/schedule';
+import { TagService } from './services/tag/tag.service';
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(
     private readonly groupService: GroupService,
     private readonly vehicleService: VehicleService,
     private readonly sessionService: SessionService,
+    private readonly tagService: TagService,
   ) {}
 
   // popolo database all'avvio
@@ -24,13 +26,18 @@ export class AppService implements OnModuleInit {
     }
     // const vehicles = await this.vehicleService.getAllVehicles();
     // for (const vehicle of vehicles) {
-    //   console.log(`${vehicle.veId} - ${vehicle.id}`);
+    //   console.log(`${vehicle.veId} con targa: ${vehicle.plate} - ${vehicle.id}`);
     //   await this.sessionService.getSessionist(
     //     vehicle.veId,
-    //     '2024-10-26T00:00:00.000Z',
-    //     '2024-10-27T00:00:00.000Z',
+    //     '2024-10-28T00:00:00.000Z',
+    //     '2024-10-29T00:00:00.000Z',
     //   );
     // }
+    await this.tagService.putTagHistory(
+      3677,
+      '2024-10-20T00:00:00.000Z',
+      '2024-10-27T00:00:00.000Z',
+    );
   }
 
   @Cron('0 0 * * *')
