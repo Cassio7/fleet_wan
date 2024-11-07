f49da0e8bae0b4de8706adbab266e3e567ed5b31
Lorenzo <lorenzo@nomail.com>
2024-11-07 11:48:51 +0000

    Home page and navbar style updated

--- frontend_fleet/src/app/components/home/home.component.ts
+++ frontend_fleet/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,14 @@ import { NavbarComponent } from "../navbar/navbar.component";
 })
 export class HomeComponent {
 
+  constructor(
+    private router: Router
+  ){}
+
+  workingCardClick(){
+    this.router.navigate(['../working-vehicles']);
+  }
+  brokenCardClick(){
+    this.router.navigate(['../broken-vehicles']);
+  }
 }
--- frontend_fleet/src/app/components/navbar/navbar.component.ts
+++ frontend_fleet/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
