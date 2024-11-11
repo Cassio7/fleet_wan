9cc68e1c58f15a992cf13bd9d8e0b8333c013891
AlessioC <AlessioC@nomail.com>
2024-11-11 10:16:14 +0000

    fix

--- backend_fleet/classes/entities/tag.entity.ts
+++ backend_fleet/classes/entities/tag.entity.ts
@@ -1,16 +1,14 @@
 import { CommonEntity } from 'classes/common/common.entity';
 import { Column, Entity, Index, OneToMany } from 'typeorm';
 import { DetectionTagEntity } from './detection_tag.entity';
+import { TagInterface } from 'classes/interfaces/tag.interface';
 
 @Entity('tags')
-export class TagEntity extends CommonEntity implements TagEntity {
+export class TagEntity extends CommonEntity implements TagInterface {
   @Column()
   @Index()
   epc: string;
 
-  @OneToMany(
-    () => DetectionTagEntity,
-    (detectiontag) => detectiontag.tag,
-  )
+  @OneToMany(() => DetectionTagEntity, (detectiontag) => detectiontag.tag)
   detectiontag: DetectionTagEntity[];
 }
