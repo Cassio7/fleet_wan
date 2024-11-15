f2a1e616029409df787efd82a5d8178a7aa34e80
AlessioC <AlessioC@nomail.com>
2024-11-15 15:55:50 +0000

    cvs parser

--- backend_fleet/src/utils/utils.ts
+++ backend_fleet/src/utils/utils.ts
@@ -1,3 +1,7 @@
+import * as fs from 'fs';
+import { parse } from 'csv-parse';
+import { NotFoundException } from '@nestjs/common';
+
 /**
  * Funzione che converte un orario del timestamp in base al fuso orario
  * @param timestamp timestamp di partenza
@@ -30,4 +34,23 @@ export function getDaysInRange(startDate, endDate) {
     currentDate.setDate(currentDate.getDate() + 1);
   }
   return dates;
+}
+
+/**
+ * Metodo per parsing del file CSV
+ * @returns
+ */
+export async function parseCsvFile(cvsPath: string): Promise<any[]> {
+  if (!fs.existsSync(cvsPath)) {
+    throw new NotFoundException(`File CSV non trovato: ${cvsPath}`);
+  }
+  return new Promise((resolve, reject) => {
+    const results: any[] = [];
+
+    fs.createReadStream(cvsPath) // Usa il percorso calcolato
+      .pipe(parse({ columns: true })) // Usa columns: true per ottenere oggetti chiave-valore
+      .on('data', (data) => results.push(data))
+      .on('end', () => resolve(results))
+      .on('error', (error) => reject(error));
+  });
 }
