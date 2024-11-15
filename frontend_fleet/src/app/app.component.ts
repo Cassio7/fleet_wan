5ccfbf8bd388dac68a4d6f4ea543d4ab2d247df3
Lorenzo <lorenzo@nomail.com>
2024-11-15 16:24:09 +0000

    login css

--- frontend_fleet/src/app/app.component.ts
+++ frontend_fleet/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
+import { filter, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,33 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'frontend_fleet';
+  private readonly destroy$: Subject<void> = new Subject<void>();
+
+
+  constructor(private router: Router){}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.setBackgroundImage(event.urlAfterRedirects);
+    });
+  }
+
+
+  setBackgroundImage(url: string): void {
+    const body = document.body;
+    if (url === '/login') {
+      body.classList.add('bg-image');
+    } else {
+      body.classList.remove('bg-image');
+    }
+  }
 }
