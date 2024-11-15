219db496138610e0c73c01d134e9cbe1f33b0f68
AlessioC <AlessioC@nomail.com>
2024-11-15 15:58:11 +0000

    new factory input data

---
+++ backend_fleet/src/factory/group.factory.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CompanyEntity } from 'classes/entities/company.entity';
+import { GroupEntity } from 'classes/entities/group.entity';
+import * as path from 'path';
+import { parseCsvFile } from 'src/utils/utils';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class GroupFactoryService {
+  private readonly cvsPath = path.resolve(process.cwd(), 'files/COMUNI.csv');
+
+  constructor(
+    @InjectRepository(GroupEntity, 'mainConnection')
+    private groupRepository: Repository<GroupEntity>,
+    @InjectRepository(CompanyEntity, 'mainConnection')
+    private companyRepository: Repository<CompanyEntity>,
+  ) {}
+
+  async createDefaultGroup(): Promise<GroupEntity[]> {
+    const groupData = await parseCsvFile(this.cvsPath);
+
+    const groupEntities = await Promise.all(
+      groupData.map(async (data) => {
+        const group = new GroupEntity();
+        group.vgId = data.vgId;
+        group.name = data.name;
+        group.company = await this.companyRepository.findOne({
+          where: {
+            id: data.companyId,
+          },
+        });
+        return group;
+      }),
+    );
+
+    return this.groupRepository.save(groupEntities);
+  }
+}
