46939faf7c454dadd111b3c88cda689132e4bc44
AlessioC <AlessioC@nomail.com>
2024-11-15 16:00:22 +0000

    update

--- backend_fleet/classes/interfaces/vehiclegroup.interface.ts
+++
@@ -1,8 +0,0 @@
-import { CommonInterface } from "classes/common/common.interface";
-import { GroupInterface } from "./group.interface";
-import { VehicleInterface } from "./vehicle.interface";
-
-export interface VehicleGroupInterface extends CommonInterface{
-  group: GroupInterface;
-  vehicle: VehicleInterface;
-}
