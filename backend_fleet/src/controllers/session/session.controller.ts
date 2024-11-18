4394bc6a187c85bf31b6d3b8c495780c777c1edf
AlessioC <AlessioC@nomail.com>
2024-11-18 16:25:48 +0000

    comment update

--- backend_fleet/src/controllers/session/session.controller.ts
+++ backend_fleet/src/controllers/session/session.controller.ts
@@ -1051,59 +1051,59 @@ export class SessionController {
     }
   }
 
-  @Post('update/:id')
-  async getHistoryList(
-    @Res() res: Response,
-    @Param() params: any,
-    @Body() body: any,
-  ) {
-    try {
-      const dateFrom = body.dateFrom;
-      const dateTo = body.dateTo;
-      // Controlla se dateFrom e dateTo sono forniti
-      if (!dateFrom || !dateTo) {
-        return res.status(400).send('Date non fornite.');
-      }
+  // @Post('update/:id')
+  // async getHistoryList(
+  //   @Res() res: Response,
+  //   @Param() params: any,
+  //   @Body() body: any,
+  // ) {
+  //   try {
+  //     const dateFrom = body.dateFrom;
+  //     const dateTo = body.dateTo;
+  //     // Controlla se dateFrom e dateTo sono forniti
+  //     if (!dateFrom || !dateTo) {
+  //       return res.status(400).send('Date non fornite.');
+  //     }
 
-      // Crea un oggetto Date dalla stringa fornita
-      const dateFrom_new = new Date(dateFrom);
-      const dateTo_new = new Date(dateTo);
+  //     // Crea un oggetto Date dalla stringa fornita
+  //     const dateFrom_new = new Date(dateFrom);
+  //     const dateTo_new = new Date(dateTo);
 
-      // Controlla se la data è valida
-      if (isNaN(dateFrom_new.getTime()) || isNaN(dateTo_new.getTime())) {
-        return res.status(400).send('Formato della data non valido.');
-      }
-      if (dateFrom_new.getTime() >= dateTo_new.getTime()) {
-        // Restituisci un errore se la condizione è vera
-        return res
-          .status(400)
-          .send(
-            'La data iniziale deve essere indietro di almeno 1 giorno dalla finale',
-          );
-      }
-      const data = await this.sessionService.getSessionist(
-        params.id,
-        dateFrom_new.toISOString(),
-        dateTo_new.toISOString(),
-      );
+  //     // Controlla se la data è valida
+  //     if (isNaN(dateFrom_new.getTime()) || isNaN(dateTo_new.getTime())) {
+  //       return res.status(400).send('Formato della data non valido.');
+  //     }
+  //     if (dateFrom_new.getTime() >= dateTo_new.getTime()) {
+  //       // Restituisci un errore se la condizione è vera
+  //       return res
+  //         .status(400)
+  //         .send(
+  //           'La data iniziale deve essere indietro di almeno 1 giorno dalla finale',
+  //         );
+  //     }
+  //     const data = await this.sessionService.getSessionist(
+  //       params.id,
+  //       dateFrom_new.toISOString(),
+  //       dateTo_new.toISOString(),
+  //     );
 
-      if (data.length > 0) {
-        // let resultMessage: string = `Aggiornata history del veicolo: ${params.id}, dal giorno ${dateFrom} al giorno ${dateTo}.\n\n`;
-        // for (const item of data) {
-        //   resultMessage += `History inserito del: ${item.timestamp}\n `;
-        // }
-        // res.status(200).send(resultMessage);
-        res.status(200).send(data);
-      } else if (data === false) {
-        res.status(200).send(`No History per veicolo con id: ${params.id}\n`);
-      } else {
-        res.status(200).send('Nessun aggiornamento');
-      }
-    } catch (error) {
-      console.error('Errore nella richiesta al db:', error);
-      res.status(500).send('Errore durante la richiesta al db');
-    }
-  }
+  //     if (data.length > 0) {
+  //       // let resultMessage: string = `Aggiornata history del veicolo: ${params.id}, dal giorno ${dateFrom} al giorno ${dateTo}.\n\n`;
+  //       // for (const item of data) {
+  //       //   resultMessage += `History inserito del: ${item.timestamp}\n `;
+  //       // }
+  //       // res.status(200).send(resultMessage);
+  //       res.status(200).send(data);
+  //     } else if (data === false) {
+  //       res.status(200).send(`No History per veicolo con id: ${params.id}\n`);
+  //     } else {
+  //       res.status(200).send('Nessun aggiornamento');
+  //     }
+  //   } catch (error) {
+  //     console.error('Errore nella richiesta al db:', error);
+  //     res.status(500).send('Errore durante la richiesta al db');
+  //   }
+  // }
 
   @Post("checkerrors/all")
   async checkErrorsAll(@Res() res: Response, @Body() body) {
