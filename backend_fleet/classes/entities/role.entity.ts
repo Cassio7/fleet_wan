ad66a6d51a1e26d788e46615f6bf87a81eb72685
AlessioC <AlessioC@nomail.com>
2024-11-20 14:15:11 +0000

    role_company relation

--- backend_fleet/classes/entities/role.entity.ts
+++ backend_fleet/classes/entities/role.entity.ts
@@ -2,6 +2,7 @@ import { CommonEntity } from 'classes/common/common.entity';
 import { RoleInterface } from 'classes/interfaces/role.interface';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { UserRoleEntity } from './user_role.entity';
+import { RoleCompanyEntity } from './role_company.entity';
 
 @Entity('roles')
 export class RoleEntity extends CommonEntity implements RoleInterface {
@@ -13,4 +14,7 @@ export class RoleEntity extends CommonEntity implements RoleInterface {
 
   @OneToMany(() => UserRoleEntity, (user_role) => user_role.role)
   user_role: UserRoleEntity[];
+
+  @OneToMany(() => RoleCompanyEntity, (user_role) => user_role.role)
+  role_company: RoleCompanyEntity[];
 }
