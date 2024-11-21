26d7366c2641143a16227eeee2575783baa69585
AlessioC <AlessioC@nomail.com>
2024-11-21 12:52:36 +0000

    getCompanyByVgId

--- backend_fleet/src/controllers/company/company.controller.ts
+++ backend_fleet/src/controllers/company/company.controller.ts
@@ -22,13 +22,28 @@ export class CompanyController {
 
   /**
    * Ritorna la società dove è presente il Veicolo cercato
-   * @param res VeId identificativo veicolo
-   * @param params
+   * @param res 
+   * @param params VeId identificativo veicolo
    */
-  @Get('/:veId')
+  @Get('vehicle/:veId')
   async getCompanyByVeId(@Res() res: Response, @Param() params: any) {
     try {
       const company = await this.companyService.getCompanyByVeId(params.veId);
+      res.status(200).json(company);
+    } catch (error) {
+      console.error('Errore nel recupero dei veicoli:', error);
+      res.status(500).send('Errore durante il recupero dei veicoli');
+    }
+  }
+  /**
+   * Ritorna la società dove è presente il gruppo cercato
+   * @param res 
+   * @param params VgId identificativo del gruppo
+   */
+  @Get('group/:vgId')
+  async getCompanyByVgId(@Res() res: Response, @Param() params: any) {
+    try {
+      const company = await this.companyService.getCompanyByVgId(params.vgId);
       res.status(200).json(company);
     } catch (error) {
       console.error('Errore nel recupero dei veicoli:', error);
