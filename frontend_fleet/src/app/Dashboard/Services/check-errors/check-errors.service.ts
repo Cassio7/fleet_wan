8a4ec99eb7b8a226b2fecedb92fbd57cbcf4dc94
Lorenzo <lorenzo@nomail.com>
2024-11-25 15:54:41 +0000

    fix check errore antenna

--- frontend_fleet/src/app/Dashboard/Services/check-errors/check-errors.service.ts
+++ frontend_fleet/src/app/Dashboard/Services/check-errors/check-errors.service.ts
@@ -24,14 +24,18 @@ export class CheckErrorsService {
     const dateTo = this.commonService.dateTo;
     let gpsAnomaly: any;
 
-    for(const s of vehicle.sessions){
-      const sessionDate = new Date(s.date);
-      if(sessionDate >= dateFrom && sessionDate <= dateTo){
-        gpsAnomaly = s.anomalies?.find((anomaly: any) => 'GPS' in anomaly);
+    if(vehicle.sessions?.length > 0){
+      for(const s of vehicle.sessions){
+        const sessionDate = new Date(s.date);
+        if(sessionDate >= dateFrom && sessionDate <= dateTo){
+          gpsAnomaly = s.anomalies?.find((anomaly: any) => 'GPS' in anomaly);
+        }
       }
     }
 
     if (gpsAnomaly) {
+      console.log(gpsAnomaly);
+      console.log(vehicle);
       return gpsAnomaly.GPS || 'Errore GPS';
     }
 
@@ -41,28 +45,32 @@ export class CheckErrorsService {
   /**
    * Controlla se è presente un errore di antenna nella sessione di oggi del veicolo preso in input
    * @param vehicle
-   * @returns
+   * @returns se viene riscontrata l'anomalia di antenna in una sessione nel range di tempo, altrimenti "null"
    */
-  checkAntennaError(vehicle: any): string | null {
+  checkAntennaError(vehicle: { anomalySessions?: { date: string; anomalies?: { antenna?: string }[] }[] }): string | boolean {
     const dateFrom = this.commonService.dateFrom;
     const dateTo = this.commonService.dateTo;
 
-    let antennaAnomaly: any;
+    // Verifica che il veicolo abbia sessioni di anomalie
+    if (vehicle.anomalySessions?.length) {
+      for (const session of vehicle.anomalySessions) {
+        const sessionDate = new Date(session.date);
 
-    for (const s of vehicle.sessions) {
-      const sessionDate = new Date(s.date);
-      if (sessionDate >= dateFrom && sessionDate <= dateTo) {
-        antennaAnomaly = s.anomalies?.find((anomaly: any) => 'antenna' in anomaly);
+        // Se la sessione è nel range di date, cerca l'anomalia "antenna"
+        if (sessionDate >= dateFrom && sessionDate <= dateTo) {
+          const antennaAnomaly = session.anomalies?.find(anomaly => anomaly.antenna);
+
+          if (antennaAnomaly) {
+            return antennaAnomaly.antenna || 'Errore antenna';
+          }
+        }
       }
     }
 
-
-    if (antennaAnomaly) {
-      return antennaAnomaly.antenna || 'Errore antenna';
-    }
-
-    return null;
+    return false;
   }
+
+
 
   /**
    * Controlla se è presente un anomalia di sessione nella sessione di oggi del veicolo preso in input
@@ -75,13 +83,18 @@ export class CheckErrorsService {
 
     let sessionAnomaly: any;
 
-    for (const s of vehicle.sessions) {
-      const sessionDate = new Date(s.date);
-      if (sessionDate >= dateFrom && sessionDate <= dateTo) {
-        sessionAnomaly = s.anomalies?.find((anomaly: any) => 'sessionEnd' in anomaly);
+    if(vehicle.sessions?.length > 0){
+      for (const s of vehicle.sessions) {
+        const sessionDate = new Date(s.date);
+        if (sessionDate >= dateFrom && sessionDate <= dateTo) {
+          sessionAnomaly = s.anomalies?.find((anomaly: any) => 'sessionEnd' in anomaly);
+        }
       }
     }
 
+    if(!vehicle.lastValidSession.period_from){
+      return "Nessuna sessione trovata"
+    }
     if (sessionAnomaly) {
       return sessionAnomaly.sessionEnd || 'Errore sessione';
     }
