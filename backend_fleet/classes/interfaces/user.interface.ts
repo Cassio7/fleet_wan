a2eb7491d81fe22ba8805883f0f86cb14af70f4f
AlessioC <AlessioC@nomail.com>
2024-12-03 14:43:19 +0000

    name + surname

--- backend_fleet/classes/interfaces/user.interface.ts
+++ backend_fleet/classes/interfaces/user.interface.ts
@@ -3,5 +3,7 @@ import { CommonInterface } from 'classes/common/common.interface';
 export interface UserInterface extends CommonInterface {
   username: string;
   email: string;
+  name: string;
+  surname: string;
   password: string;
 }
