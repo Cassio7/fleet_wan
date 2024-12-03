ccae62eb5fc1ccac820c098a6bc17eb8441857c9
AlessioC <AlessioC@nomail.com>
2024-12-03 14:43:56 +0000

    new functions + guards

--- backend_fleet/src/controllers/user/user.controller.ts
+++ backend_fleet/src/controllers/user/user.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
+import { AuthGuard } from 'src/guard/auth.guard';
 import { UserService } from 'src/services/user/user.service';
+import { AdminGuard } from 'src/guard/admin.guard';
 
 @Controller('user')
 export class UserController {
@@ -10,14 +20,52 @@ export class UserController {
    * API per restituire tutti gli utenti
    * @param res
    */
+  @UseGuards(AuthGuard, AdminGuard)
   @Get()
   async getAllUsers(@Res() res: Response) {
     try {
       const users = await this.userService.getAllUsers();
-      res.status(200).json(users);
+      if (users) res.status(200).json(users);
+      else res.status(404).json({ message: 'Nessun utente trovato' });
     } catch (error) {
-      console.error('Errore nel recupero dei realtimes:', error);
-      res.status(500).send('Errore durante il recupero dei realtimes');
+      console.error('Errore nel recupero degli utenti:', error);
+      res.status(500).json({ message: 'Errore nel recupero degli utenti' });
+    }
+  }
+
+  /**
+   * API per restituire un utente in base all'id
+   * @param res
+   * @param params id utente
+   */
+  @UseGuards(AuthGuard, AdminGuard)
+  @Get(':id')
+  async getUserById(@Res() res: Response, @Param() params: any) {
+    try {
+      const users = await this.userService.getUserById(params.id);
+      if (users) res.status(200).json(users);
+      else res.status(404).json({ message: 'Utente non trovato' });
+    } catch (error) {
+      console.error("Errore nel recupero dell'utente:", error);
+      res.status(500).json({ message: "Errore nel recupero dell'utente" });
+    }
+  }
+
+  /**
+   * API per restituire un utente in base all'username
+   * @param res
+   * @param body username utente
+   */
+  @UseGuards(AuthGuard, AdminGuard)
+  @Post('username')
+  async getUserByUsername(@Res() res: Response, @Body() body: any) {
+    try {
+      const users = await this.userService.getUserByUsername(body.username);
+      if (users) res.status(200).json(users);
+      else res.status(404).json({ message: 'Utente non trovato' });
+    } catch (error) {
+      console.error("Errore nel recupero dell'utente:", error);
+      res.status(500).json({ message: "Errore nel recupero dell'utente" });
     }
   }
 }
