0ff68ebb867bcf6675a53cc67727bc91a888e865
AlessioC <AlessioC@nomail.com>
2024-12-10 12:32:30 +0000

    guard implement

--- backend_fleet/src/controllers/user/user.controller.ts
+++ backend_fleet/src/controllers/user/user.controller.ts
@@ -10,9 +10,14 @@ import {
 import { Response } from 'express';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { UserService } from 'src/services/user/user.service';
-import { AdminGuard } from 'src/guard/admin.guard';
+import { RolesGuard } from 'src/guard/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'classes/enum/role.enum';
+import { UserDTO } from 'classes/dtos/user.dto';
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('user')
+@Roles(Role.Admin)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -20,7 +25,6 @@ export class UserController {
    * API per restituire tutti gli utenti
    * @param res
    */
-  @UseGuards(AuthGuard, AdminGuard)
   @Get()
   async getAllUsers(@Res() res: Response) {
     try {
@@ -38,13 +42,19 @@ export class UserController {
    * @param res
    * @param params id utente
    */
-  @UseGuards(AuthGuard, AdminGuard)
   @Get(':id')
   async getUserById(@Res() res: Response, @Param() params: any) {
     try {
-      const users = await this.userService.getUserById(params.id);
-      if (users) res.status(200).json(users);
-      else res.status(404).json({ message: 'Utente non trovato' });
+      const user = await this.userService.getUserById(params.id);
+      if (user) {
+        const userDTO = new UserDTO();
+        userDTO.email = user.email;
+        userDTO.name = user.name;
+        userDTO.surname = user.surname;
+        userDTO.username = user.username;
+        userDTO.role = user.role.name;
+        res.status(200).json(userDTO);
+      } else res.status(404).json({ message: 'Utente non trovato' });
     } catch (error) {
       console.error("Errore nel recupero dell'utente:", error);
       res.status(500).json({ message: "Errore nel recupero dell'utente" });
@@ -56,13 +66,19 @@ export class UserController {
    * @param res
    * @param body username utente
    */
-  @UseGuards(AuthGuard, AdminGuard)
   @Post('username')
   async getUserByUsername(@Res() res: Response, @Body() body: any) {
     try {
-      const users = await this.userService.getUserByUsername(body.username);
-      if (users) res.status(200).json(users);
-      else res.status(404).json({ message: 'Utente non trovato' });
+      const user = await this.userService.getUserByUsername(body.username);
+      if (user) {
+        const userDTO = new UserDTO();
+        userDTO.email = user.email;
+        userDTO.name = user.name;
+        userDTO.surname = user.surname;
+        userDTO.username = user.username;
+        userDTO.role = user.role.name;
+        res.status(200).json(userDTO);
+      } else res.status(404).json({ message: 'Utente non trovato' });
     } catch (error) {
       console.error("Errore nel recupero dell'utente:", error);
       res.status(500).json({ message: "Errore nel recupero dell'utente" });
