97521bef27a6403d4f59d5f09b82761de71a90f7
AlessioC <AlessioC@nomail.com>
2024-12-12 16:43:21 +0000

    new column allestimento

--- backend_fleet/classes/entities/vehicle.entity.ts
+++ backend_fleet/classes/entities/vehicle.entity.ts
@@ -50,6 +50,9 @@ export class VehicleEntity extends CommonEntity implements VehicleInterface {
   @Index()
   isRFIDReader: boolean;
 
+  @Column({ type: 'varchar', nullable: true })
+  allestimento: string;
+
   @Column()
   profileId: number;
 
