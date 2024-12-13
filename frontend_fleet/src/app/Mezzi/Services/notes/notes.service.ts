e6780c06347addaa848a0c6d7c0a35405e27f11c
Lorenzo <lorenzo@nomail.com>
2024-12-13 10:18:12 +0000

    notes service

---
+++ frontend_fleet/src/app/Mezzi/Services/notes/notes.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Note } from '../../../Models/Note';
+import { CommonService } from '../../../Common-services/common service/common.service';
+import { SessionStorageService } from '../../../Common-services/sessionStorage/session-storage.service';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  constructor(
+    private commonService: CommonService,
+    private http: HttpClient
+  ) { }
+
+  saveNoteInDB(note: Note){
+    const body = {
+      userId: note.userId,
+      vehicleId: note.vehicle.veId,
+      content: note.content
+    }
+    return this.http.post(`${this.commonService.url}/notes/update`, body);
+  }
+
+}
