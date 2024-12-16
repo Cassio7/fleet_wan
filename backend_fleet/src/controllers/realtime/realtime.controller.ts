50583a89e035f3b68679bdac6e48ed32dc742d60
AlessioC <AlessioC@nomail.com>
2024-12-16 16:29:48 +0000

    realtime set and get all, last, by veid include role access and filter

--- backend_fleet/src/controllers/realtime/realtime.controller.ts
+++ backend_fleet/src/controllers/realtime/realtime.controller.ts
@@ -10,8 +10,8 @@ import { Roles } from 'src/decorators/roles.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RolesGuard } from 'src/guard/roles.guard';
 import { AssociationService } from 'src/services/association/association.service';
-import { CompanyService } from 'src/services/company/company.service';
 import { RealtimeService } from 'src/services/realtime/realtime.service';
+import { VehicleService } from 'src/services/vehicle/vehicle.service';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Roles(Role.Admin, Role.Responsabile, Role.Capo)
@@ -19,21 +19,32 @@ import { RealtimeService } from 'src/services/realtime/realtime.service';
 export class RealtimeController {
   private readonly nominatimUrl = 'https://nominatim.openstreetmap.org/reverse';
   constructor(
-    private readonly realTimeService: RealtimeService,
-    private readonly companyService: CompanyService,
+    private readonly realtimeService: RealtimeService,
     private readonly associationService: AssociationService,
+    private readonly vehicleService: VehicleService,
     @InjectRedis() private readonly redis: Redis,
   ) {}
 
   /**
    * Recupera tutti i realtimes di tutti i veicoli salvati
+   * @param req
    * @param res
    */
   @Get()
-  async getAllTimes(@Res() res: Response) {
+  async getAllTimes(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+  ) {
     try {
-      const times = await this.realTimeService.getAllTimes();
-      if (times) res.status(200).json(times);
+      const vehicles = (await this.associationService.getVehiclesByUserRole(
+        req.user.id,
+      )) as VehicleEntity[];
+      if (!vehicles || vehicles.length === 0) {
+        return res.status(404).json({ message: 'Nessun Veicolo associato' });
+      }
+      const vehicleIds = vehicles.map((vehicle) => vehicle.veId);
+      const times = await this.realtimeService.getTimesByVeId(vehicleIds);
+      if (times.length > 0) res.status(200).json(times);
       else
         res.status(404).json({
           message: 'Nessun realtime recuperato',
@@ -43,60 +54,122 @@ export class RealtimeController {
       res.status(500).json({ message: 'Errore nel recupero dei realtimes' });
     }
   }
-  @Get('resolvelast')
-  async getResolveLast(
+
+  /**
+   * Recupera l'ultimo realtime disponibile di tutti i veicoli in base all'utente
+   * @param req User
+   * @param res
+   * @returns
+   */
+  @Get('last')
+  async getLast(
     @Req() req: Request & { user: UserFromToken },
     @Res() res: Response,
   ) {
     try {
-      const vehicles = await this.associationService.getVehiclesByUserRole(
+      const vehicles = (await this.associationService.getVehiclesByUserRole(
         req.user.id,
-      );
-      if (vehicles) {
-        const vehicleIds = vehicles.map(
-          (vehicle) => (vehicle as VehicleEntity).veId,
-        );
-        const realtimes = await this.realTimeService.getLastByVeId(vehicleIds);
-        // Raggruppa gli ultimi realtime per veicolo
-        const latestRealtimes = realtimes.reduce((acc, current) => {
-          const existing = acc.find(
-            (item) => item.vehicle.veId === current.vehicle.veId,
-          );
-          if (!existing || current.timestamp > existing.timestamp) {
-            // Rimuovi l'elemento esistente se trovato e aggiungi quello corrente
-            return [
-              ...acc.filter(
-                (item) => item.vehicle.veId !== current.vehicle.veId,
-              ),
-              current,
-            ];
-          }
-          return acc;
-        }, []);
-
-        return res.status(200).json(latestRealtimes);
-      } else {
-        return res.status(404).json({ message: 'Nessun realtime trovato' });
+      )) as VehicleEntity[];
+      if (!vehicles || vehicles.length === 0) {
+        return res.status(404).json({ message: 'Nessun Veicolo associato' });
       }
+      const latestRealtimes = [];
+      const vehicleIds = vehicles.map((vehicle) => vehicle.veId);
+      for (const id of vehicleIds) {
+        const key = `realtime:${id}`;
+        latestRealtimes.push(JSON.parse(await this.redis.get(key)));
+      }
+      // Verifica se `latestRealtimes` è vuoto
+      if (latestRealtimes.length === 0) {
+        return res
+          .status(404)
+          .json({ message: 'Nessun dato trovato per i veicoli richiesti.' });
+      }
+      res.status(200).json(latestRealtimes);
     } catch (error) {
       console.error('Errore nel recupero dei realtimes:', error);
       res.status(500).json({ message: 'Errore nel recupero dei realtimes' });
     }
   }
+
+  /**
+   * Fa una chiamata al servizio SOAP per recuperare i realtimes in base al vgId
+   * @param res
+   * @param params vgId identificativo del gruppo
+   */
+  @Get('update')
+  async updateRealtime(@Res() res: Response) {
+    try {
+      await this.realtimeService.clearRealtime();
+      const realtimePromises = [
+        this.realtimeService.setRealtime(254, 313), // Gesenu principale
+        this.realtimeService.setRealtime(305, 650), // TSA principale
+        this.realtimeService.setRealtime(324, 688), // Fiumicino principale
+      ];
+      const results = await Promise.all(realtimePromises);
+      // Controlla se almeno una delle chiamate è andata a buon fine
+      const allUpdated = results.some((result) => result);
+      const vehicles = await this.vehicleService.getAllVehicles();
+      if (!vehicles) {
+        res.status(404).json({ message: 'Nessun aggiornamento disponibile' });
+      }
+      const vehicleIds = vehicles.map((vehicle) => vehicle.veId);
+      const realtimes = await this.realtimeService.getTimesByVeId(vehicleIds);
+      const latestRealtimes =
+        await this.realtimeService.calculateLastValid(realtimes);
+      await this.realtimeService.setLastValid(latestRealtimes);
+      if (allUpdated) {
+        res
+          .status(200)
+          .json({ message: 'Aggiornamento completato con successo' });
+      } else {
+        res.status(404).json({ message: 'Nessun aggiornamento disponibile' });
+      }
+    } catch (error) {
+      console.error('Errore nella richiesta SOAP:', error);
+      res
+        .status(500)
+        .json({ message: 'Errore durante la richiesta al servizio SOAP' });
+    }
+  }
+
   /**
    * Recupera i realtime in base al veId
    * @param res
    * @param params veId Identificativo del veicolo
    */
   @Get('/:veId')
-  async getTimeByVeId(@Res() res: Response, @Param() params: any) {
+  async getTimeByVeId(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+    @Param() params: any,
+  ) {
     try {
-      const times = await this.realTimeService.getTimesByVeId(params.veId);
-      if (times) res.status(200).json(times);
-      else
-        res.status(404).json({
-          message: 'Nessun realtime recuperato per Veicolo: ' + params.veId,
+      const vehicles = (await this.associationService.getVehiclesByUserRole(
+        req.user.id,
+      )) as VehicleEntity[];
+
+      if (!vehicles || !Array.isArray(vehicles)) {
+        return res.status(404).json({ message: 'Nessun Veicolo associato' });
+      }
+
+      const vehicle = vehicles.find(
+        (element) => element.veId === Number(params.veId),
+      );
+
+      if (!vehicle) {
+        return res.status(404).json({
+          message: `Non hai i permessi per visualizzare il veicolo con VeId: ${params.veId}`,
         });
+      }
+      const times = await this.realtimeService.getTimesByVeId([vehicle.veId]);
+      if (!times || times.length === 0) {
+        return res.status(404).json({
+          message: `Nessun realtime recuperato per Veicolo: ${params.veId}`,
+        });
+      }
+
+      res.status(200).json(times);
     } catch (error) {
       console.error('Errore nel recupero dei realtimes:', error);
       res.status(500).json({ message: 'Errore nel recupero dei realtimes' });
@@ -111,7 +184,7 @@ export class RealtimeController {
   @Get('resolve/:veId')
   async getResolvedByVeId(@Res() res: Response, @Param() params: any) {
     try {
-      const times = await this.realTimeService.getTimesByVeId(params.veId);
+      const times = await this.realtimeService.getTimesByVeId(params.veId);
       const response = [];
       for (const time of times) {
         const cacheKey = `pos:${time.latitude}:${time.longitude}`;
@@ -137,34 +210,6 @@ export class RealtimeController {
       res
         .status(500)
         .json({ message: 'Errore durante il recupero dei realtimes' });
-    }
-  }
-
-  /**
-   * Fa una chiamata al servizio SOAP per recuperare i realtimes in base al vgId
-   * @param res
-   * @param params vgId identificativo del gruppo
-   */
-  @Get('update/:vgId')
-  async getRealTimeList(@Res() res: Response, @Param() params: any) {
-    try {
-      const company = await this.companyService.getCompanyByVgId(params.vgId);
-      const data = await this.realTimeService.getRealTimeList(
-        company.suId,
-        params.vgId,
-      );
-      if (data) {
-        res
-          .status(200)
-          .json({ message: `Aggiornato il gruppo: ${company.group[0].name}` });
-      } else {
-        res.status(404).json({ message: 'Nessun aggiornamento' });
-      }
-    } catch (error) {
-      console.error('Errore nella richiesta SOAP:', error);
-      res
-        .status(500)
-        .json({ message: 'Errore durante la richiesta al servizio SOAP' });
     }
   }
 }
--- backend_fleet/src/services/realtime/realtime.service.ts
+++ backend_fleet/src/services/realtime/realtime.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import { DataSource, Repository, In } from 'typeorm';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+import { RealtimeDTO } from 'classes/dtos/realtime.dto';
+import { VehicleDTO } from 'classes/dtos/vehicle.dto';
 import { RealtimePositionEntity } from 'classes/entities/realtime_position.entity';
-import { parseStringPromise } from 'xml2js';
-import { createHash } from 'crypto';
 import { VehicleEntity } from 'classes/entities/vehicle.entity';
 import { convertHours } from 'src/utils/utils';
+import { DataSource, In, Repository } from 'typeorm';
+import { parseStringPromise } from 'xml2js';
+
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class RealtimeService {
@@ -17,6 +21,7 @@ export class RealtimeService {
     private readonly realtimeRepository: Repository<RealtimePositionEntity>,
     @InjectDataSource('mainConnection')
     private readonly connection: DataSource,
+    @InjectRedis() private readonly redis: Redis,
   ) {}
 
   // Prepara la richiesta SOAP
@@ -36,7 +41,7 @@ export class RealtimeService {
       </soapenv:Envelope>`;
   }
 
-  async getRealTimeList(suId: number, vgId: number): Promise<any> {
+  async setRealtime(suId: number, vgId: number): Promise<any> {
     const methodName = 'realTimePositions';
     const requestXml = this.buildSoapRequest(methodName, suId, vgId);
     const headers = {
@@ -73,7 +78,6 @@ export class RealtimeService {
     try {
       await queryRunner.connect();
       await queryRunner.startTransaction();
-      await queryRunner.manager.getRepository(RealtimePositionEntity).clear();
       const response = await axios.post(this.serviceUrl, requestXml, {
         headers,
       });
@@ -161,46 +165,111 @@ export class RealtimeService {
       console.error('Errore nella richiesta SOAP:', error);
     }
   }
-  /**
-   * Ritorna tutti i realtimes di tutti i veicoli
-   * @returns
-   */
-  async getAllTimes(): Promise<any> {
-    const times = await this.realtimeRepository.find({
-      relations: {
-        vehicle: true,
-      },
-    });
-    return times;
-  }
 
   /**
    * Ritorna tutti i realtimes in base al VeId inserito
    * @param id VeId identificatico del veicolo
-   * @returns
+   * @returns Raggruppa i realtime per veicolo usando i DTO
    */
-  async getTimesByVeId(id: number): Promise<any> {
-    const times = await this.realtimeRepository.find({
-      relations: {
-        vehicle: true,
-      },
-      where: { vehicle: { veId: id } },
-    });
-    return times;
-  }
-
-  /**
-   * Ritorna tutti i realtimes in base ai VeId
-   * @param id array di VeId
-   * @returns
-   */
-  async getLastByVeId(id: number[]): Promise<any> {
+  async getTimesByVeId(id: number[]): Promise<any> {
     const times = await this.realtimeRepository.find({
       relations: {
         vehicle: true,
       },
       where: { vehicle: { veId: In(id) } },
     });
-    return times;
+    // Creo DTO e formatto un output corretto
+    return times.reduce(
+      (acc, time) => {
+        // Trova l'indice del veicolo esistente o crea un nuovo gruppo
+        let vehicleGroup = acc.find(
+          (group) => group.vehicle.veId === time.vehicle.veId,
+        );
+
+        if (!vehicleGroup) {
+          const vehicleDTO = new VehicleDTO();
+          vehicleDTO.plate = time.vehicle.plate;
+          vehicleDTO.veId = time.vehicle.veId;
+
+          vehicleGroup = {
+            vehicle: vehicleDTO,
+            realtime: [],
+          };
+          acc.push(vehicleGroup);
+        }
+
+        const realtimeDTO = new RealtimeDTO();
+        realtimeDTO.timestamp = time.timestamp;
+        realtimeDTO.row_number = time.row_number;
+        realtimeDTO.status = time.status;
+        realtimeDTO.latitude = time.latitude;
+        realtimeDTO.longitude = time.longitude;
+        realtimeDTO.nav_mode = time.nav_mode;
+        realtimeDTO.speed = time.speed;
+        realtimeDTO.direction = time.direction;
+        vehicleGroup.realtime.push(realtimeDTO);
+
+        return acc;
+      },
+      [] as Array<{ vehicle: VehicleDTO; realtime: RealtimeDTO[] }>,
+    );
+  }
+
+  /**
+   * Pulisco la tabella dai vecchi dati salvati
+   */
+  async clearRealtime() {
+    const queryRunner = this.connection.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      await queryRunner.manager.getRepository(RealtimePositionEntity).clear();
+      // fa ripartire gli id da 1
+      await queryRunner.query(
+        'ALTER SEQUENCE realtime_positions_id_seq RESTART WITH 1',
+      );
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      console.error('Errore nella pulizia della tabella: ', error);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  /**
+   * Filtra soltanto la posizione più recente senza lat e long = 0
+   * @param realtimes Oggetto contenente tutte i veicoli e le relative posizioni
+   * @returns
+   */
+  async calculateLastValid(realtimes: any) {
+    const latestRealtimes = realtimes.map((group) => {
+      // Trova il primo realtime valido partendo dal più grande (già ordinato per timestamp)
+      for (const realtime of group.realtime) {
+        if (realtime.latitude !== 0 && realtime.longitude !== 0) {
+          return {
+            vehicle: group.vehicle,
+            realtime: realtime,
+          };
+        }
+      }
+      // Se nessun realtime ha coordinate valide, prendo il primo
+      return {
+        vehicle: group.vehicle,
+        realtime: group.realtime[0],
+      };
+    });
+    return latestRealtimes;
+  }
+
+  /**
+   * Imposta i realtime sulla cache di redis per recupero veloce
+   * @param realtimes
+   */
+  async setLastValid(realtimes: any) {
+    for (const realtime of realtimes) {
+      const key = `realtime:${realtime.vehicle.veId}`;
+      await this.redis.set(key, JSON.stringify(realtime));
+    }
   }
 }
