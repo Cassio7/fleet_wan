d45ab6539db7a4df0e17e20fe95de576337ea922
Lorenzo <lorenzo@nomail.com>
2024-12-16 15:59:51 +0000

    login tries handling

--- frontend_fleet/src/app/Common-components/login/login.component.ts
+++ frontend_fleet/src/app/Common-components/login/login.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
-import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, signal} from '@angular/core';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { Router, RouterModule } from '@angular/router';
-import { SessionStorageService } from '../../Common-services/sessionStorage/session-storage.service';
 import { LoginService } from '../../Common-services/login service/login.service';
 import { CookiesService } from '../../Common-services/cookies service/cookies.service';
-import { Subject, takeUntil } from 'rxjs';
+import { count, Subject, takeUntil } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
@@ -23,18 +23,31 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatTooltipModule,
     RouterModule,
+    MatProgressSpinnerModule,
     MatIconModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit, AfterViewInit, OnDestroy{
   private readonly destroy$: Subject<void> = new Subject<void>();
-  loginError: boolean = false;
+
+  credentialsError: boolean = false;
+  credentialsWarning: boolean = false;
+  loginSuccess: boolean = false;
+
   loginForm!: FormGroup;
 
   isCapsActive: boolean = false;
+
+  maxAttempts: number = 3; //tentativi massimi prima dell'inizio dei timeout
+  maxTimeoutTime: number = 90; //tempo massimo di timeout
+
+
+  attempts: number = 0;
+  timeoutTime: number = 0;
+  timeoutCountdown: number = 1;
 
   constructor(
     private router: Router,
@@ -53,25 +66,136 @@ export class LoginComponent{
 
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.attempts = parseInt(this.cookiesService.getCookie("attempts")) || 0;
+    this.timeoutTime = parseInt(this.cookiesService.getCookie("timeoutTime")) || 0;
+  }
+
+  ngAfterViewInit(): void {
+    this.attempts = parseInt(this.cookiesService.getCookie("attempts")) || 0;
+    this.timeoutTime = parseInt(this.cookiesService.getCookie("timeoutTime")) || 0;
+    const currentTimeout = parseInt(this.cookiesService.getCookie('currentTimeout'));
+
+    if(currentTimeout){
+      // this.setBlockingCountdown(currentTimeout);
+    }
+
+    this.credentialsWarning = this.checkLastAttempt(this.attempts);
+    this.cd.detectChanges();
+  }
+
+  /**
+   * Effettua e gestisce gli errori di login
+   */
   login(){
     if(this.loginForm.valid){
       this.loginService.login(this.loginForm.value).pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
           this.cookiesService.setCookie("user", response.access_token);
-          this.loginService.login$.next();
-          this.router.navigate(['/dashboard']);
+          this.deleteCookies();
+          this.loginSuccess = true;
+          this.attempts = 0;
+          this.cd.detectChanges();
+          setTimeout(() => {
+            this.loginService.login$.next();
+            this.router.navigate(['/dashboard']);
+          }, 3000);
         },
         error: error => {
-          console.error("Errore nel login: ", error);
-          this.loginError = true;
+          this.attempts++;
+          this.cookiesService.setCookie("attempts", this.attempts.toString());
+
+          if(this.credentialsWarning){
+            this.credentialsWarning = false;
+          }
+          if(this.attempts == this.maxAttempts){
+            this.credentialsWarning = true;
+          }
           this.cd.detectChanges();
+
+          if(this.attempts <= this.maxAttempts){
+            this.showCredentialsError(2000); //mostra errore credenziali
+          }else{
+            this.timeoutUser(); //metti l'utente in timeout
+          }
         }
       });
-      // this.router.navigate(['dashboard']); //navigazione alla pagina principale
     }
   }
 
+  /**
+   * Mostra l'errore delle credenziali
+   * @param mills millisecondi per i quali deve essere mostrato l'errore
+   */
+  showCredentialsError(mills: number){
+    this.credentialsError = true; //mostra errore di credenziali
+    this.cd.detectChanges();
+    setTimeout(() => {
+      this.credentialsError = false; //nascondi errore credenziali
+      this.cd.detectChanges();
+    }, mills);
+  }
+
+  /**
+   * Mette l'utente in timeout
+   */
+  timeoutUser() {
+    this.timeoutTime += 30000; //incremento della durata del timer di 30 secondi
+    this.cookiesService.setCookie("timeoutTime", this.timeoutTime.toString());
+
+    //se il tempo calcolato è più del tempo massimo di timeout, imposta timeout a tempo massimo
+    this.timeoutTime > this.maxTimeoutTime * 1000 ? this.timeoutCountdown = this.maxTimeoutTime * 1000 : this.timeoutCountdown = this.timeoutTime;
+    this.setBlockingCountdown(this.timeoutCountdown);//imposta countdown di blocco
+  }
+
+  /**
+   * Imposta il timeout bloccante e lo visualizza
+   * @param countdown secondi per il timer
+   */
+  private setBlockingCountdown(countdown: number){
+    this.credentialsError = true; //mostra errore credenziali
+    this.timeoutCountdown = countdown;
+
+    const timer = setInterval(() => {
+      this.timeoutCountdown -= 1000; //decremento del countdown di 1 secondo
+
+      this.cookiesService.setCookie("currentTimeout", this.timeoutCountdown.toString());
+      this.cd.detectChanges();
+
+      if (this.timeoutCountdown <= 0) {
+        clearInterval(timer); //blocca il timer una volta finito
+
+        this.cookiesService.deleteCookie("currentTimeout");
+        this.credentialsError = false;//nascondi errore credenziali
+
+        this.cd.detectChanges();
+      }
+    }, 1000);
+  }
+
+  private checkLastAttempt(attempts: number){
+    if(attempts == this.maxAttempts){
+      return true;
+    }
+    return false;
+  }
+
+  private deleteCookies(){
+    this.cookiesService.deleteCookie("timeoutTime");
+    this.cookiesService.deleteCookie("attempts");
+    this.cookiesService.deleteCookie("currentTimeout");
+  }
+
+  /**
+   * Controlla se il caps lock è attivo alla pressione di un tasto mentre ci si trova in un campo di input di testo
+   * @param event evento
+   */
   checkCapslock(event: KeyboardEvent): void {
     this.isCapsActive = event.getModifierState('CapsLock');
   }
