c5a4050b34437ff8cc0737bb8327b832d41971b9
Lorenzo <lorenzo@nomail.com>
2024-12-17 14:08:32 +0000

    basic navabar paths

--- frontend_fleet/src/app/Common-components/navbar/navbar.component.ts
+++ frontend_fleet/src/app/Common-components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterModule } from '@angular/router';
-import { Subject } from 'rxjs';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Subject, filter } from 'rxjs';
 import { CommonService } from '../../Common-services/common service/common.service';
 
 @Component({
@@ -21,12 +21,33 @@ import { CommonService } from '../../Common-services/common service/common.servi
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit{
+  currentPage: string = '';
+  icon: string = '';
   constructor(
     private commonService: CommonService,
     private router: Router
   ){}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(()=>{
+      const url = this.router.url;
+      switch(url){
+        case '/dashboard':
+          this.currentPage = "Dashboard";
+          this.icon = "dashboard";
+          break;
+        case '/home-mezzi':
+          this.currentPage = "Mezzi";
+          this.icon = "local_shipping";
+          break;
+        default:
+          this.currentPage = "Dashboard";
+          this.icon = "dashboard";
+      }
+    });
+  }
 
   logout(){
     localStorage.removeItem("user");
