fdcfa22a320289f96cca750976d86403acaa4bab
AlessioC <AlessioC@nomail.com>
2024-12-17 15:19:23 +0000

    admin lower case

--- backend_fleet/src/factory/user.factory.ts
+++ backend_fleet/src/factory/user.factory.ts
@@ -21,7 +21,7 @@ export class UserFactoryService {
       {
         name: null,
         surname: null,
-        username: 'Admin',
+        username: 'admin',
         email: this.configService.get<string>('ADMIN_EMAIL'),
         password: this.configService.get<string>('USER_PASSWORD'),
         role: role[0],
