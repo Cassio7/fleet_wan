304b8516200ca0903c45ff076b295f5d2a40c81a
Lorenzo <lorenzo@nomail.com>
2024-12-20 10:23:02 +0000

    kanban gps styling & add & remove functions

--- frontend_fleet/src/app/Dashboard/Components/kanban-gps/kanban-gps.component.ts
+++ frontend_fleet/src/app/Dashboard/Components/kanban-gps/kanban-gps.component.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { KanbanGpsService } from '../../Services/kanban-gps/kanban-gps.service';
+import { MatButtonModule } from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-kanban-gps',
   standalone: true,
   imports: [
     CommonModule,
-    DragDropModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatListModule
   ],
   templateUrl: './kanban-gps.component.html',
   styleUrl: './kanban-gps.component.css'
 })
 export class KanbanGpsComponent {
+  workingItems: string[] = [];
+  warningItems: string[] = [];
+  errorItems: string[] = [];
 
+  newWorkingItem: string = '';
+  newWarningItem: string = '';
+  newErrorItem: string = '';
+
+  constructor(public kanbanGpsService: KanbanGpsService){}
+
+  addItem(column: 'working' | 'warning' | 'error') {
+    this.kanbanGpsService.addItem(column);
+  }
+
+  removeItem(column: 'working' | 'warning' | 'error', item: string) {
+    this.kanbanGpsService.removeItem(column, item);
+  }
 }
--- frontend_fleet/src/app/Dashboard/Services/kanban-gps/kanban-gps.service.ts
+++ frontend_fleet/src/app/Dashboard/Services/kanban-gps/kanban-gps.service.ts
@@ -6,8 +6,63 @@ import { BehaviorSubject, Subject } from 'rxjs';
 })
 export class KanbanGpsService {
   private _loadKanbanGps$: BehaviorSubject<string> = new BehaviorSubject<string>("");
+  workingItems: string[] = [];
+  warningItems: string[] = [];
+  errorItems: string[] = [];
+
+  newWorkingItem: string = '';
+  newWarningItem: string = '';
+  newErrorItem: string = '';
 
   constructor() { }
+
+  /**
+   * Aggiunge un item ad una colonna del kanban GPS
+   * @param column colonna sulla quale aggiungere
+   */
+  addItem(column: 'working' | 'warning' | 'error') {
+    switch (column) {
+      case 'working':
+        if (this.newWorkingItem.trim()) {
+          this.workingItems.push(this.newWorkingItem.trim());
+          this.newWorkingItem = '';
+        }
+        break;
+      case 'warning':
+        if (this.newWarningItem.trim()) {
+          this.warningItems.push(this.newWarningItem.trim());
+          this.newWarningItem = '';
+        }
+        break;
+      case 'error':
+        if (this.newErrorItem.trim()) {
+          this.errorItems.push(this.newErrorItem.trim());
+          this.newErrorItem = '';
+        }
+        break;
+    }
+  }
+
+  /**
+   * Rimuove un item da una colonna del kanban GPS
+   * @param column colonna dalla quale rimuovere
+   * @param item item da rimuovere
+   */
+  removeItem(column: 'working' | 'warning' | 'error', item: string) {
+    switch (column) {
+      case 'working':
+        this.workingItems = this.workingItems.filter(i => i !== item);
+        break;
+      case 'warning':
+        this.warningItems = this.warningItems.filter(i => i !== item);
+        break;
+      case 'error':
+        this.errorItems = this.errorItems.filter(i => i !== item);
+        break;
+    }
+  }
+
+
 
   public get loadKanbanGps$(): BehaviorSubject<string> {
     return this._loadKanbanGps$;
