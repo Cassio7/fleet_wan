4b8f37bcbaa0c686dff1e4c9ccbc3a320bb2c71c
Lorenzo <lorenzo@nomail.com>
2024-12-27 14:19:22 +0000

    username & role visualization

--- frontend_fleet/src/app/Common-components/navbar/navbar.component.ts
+++ frontend_fleet/src/app/Common-components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -7,6 +7,8 @@ import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { Subject, filter } from 'rxjs';
 import { CommonService } from '../../Common-services/common service/common.service';
 import { LoginService } from '../../Common-services/login service/login.service';
+import { CookiesService } from '../../Common-services/cookies service/cookies.service';
+import { AuthService } from '../../Common-services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -22,12 +24,18 @@ import { LoginService } from '../../Common-services/login service/login.service'
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit, AfterViewInit{
   currentPage: string = '';
   icon: string = '';
+  username: string = "";
+  role: string = "";
+
   constructor(
     private loginService: LoginService,
-    private router: Router
+    private cookieService: CookiesService,
+    private authService: AuthService,
+    private router: Router,
+    private cd: ChangeDetectorRef
   ){}
 
   ngOnInit(): void {
@@ -48,6 +56,18 @@ export class NavbarComponent implements OnInit{
           this.icon = "dashboard";
       }
     });
+  }
+
+  ngAfterViewInit(): void {
+    const access_token_raw = this.cookieService.getCookie("user");
+    const access_token = this.authService.decodeToken(access_token_raw);
+    this.username = access_token.username;
+    switch(access_token.role){
+      case 1:
+        this.role = "Admin";
+        break;
+    }
+    this.cd.detectChanges();
   }
 
   logout(){
