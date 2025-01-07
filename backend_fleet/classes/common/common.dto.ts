0bfcb37265a9aa35dab4a0399a8301938dba342c
AlessioC <AlessioC@nomail.com>
2025-01-07 12:35:34 +0000

    add transformer and validator

--- backend_fleet/classes/common/common.dto.ts
+++ backend_fleet/classes/common/common.dto.ts
@@ -1,11 +1,17 @@
+import { IsDate, IsNumber, IsString } from 'class-validator';
+import { Exclude } from 'class-transformer';
 import { CommonInterface } from './common.interface';
 
-export class CommonDto implements CommonInterface {
-    
+export class CommonDTO implements CommonInterface {
+  @IsNumber()
   id: number;
+  @IsString()
+  @Exclude()
   key: string;
-
+  @IsDate()
   createdAt: Date;
+  @IsDate()
   updatedAt: Date;
+  @IsNumber()
   version: number;
 }
