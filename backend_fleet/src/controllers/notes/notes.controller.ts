c1c6cbccc44668315dfbd10f49827ad2a2b6b8e3
AlessioC <AlessioC@nomail.com>
2025-01-08 12:32:03 +0000

    note CRUD operations

--- backend_fleet/src/controllers/notes/notes.controller.ts
+++ backend_fleet/src/controllers/notes/notes.controller.ts
@@ -1,58 +1,164 @@
-import { Controller, Post, Body, Get, Param, Put, Delete, Res } from '@nestjs/common';
-import { NoteDto } from 'classes/dtos/note.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Req,
+  Res,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { Role } from 'classes/enum/role.enum';
+import { UserFromToken } from 'classes/interfaces/userToken.interface';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Response } from 'express';
+import { AuthGuard } from 'src/guard/auth.guard';
+import { RolesGuard } from 'src/guard/roles.guard';
 import { NotesService } from 'src/services/notes/notes.service';
+import { NoteDto } from 'classes/dtos/note.dto';
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('notes')
 export class NotesController {
-    constructor(private readonly notesService: NotesService) {}
+  constructor(private readonly notesService: NotesService) {}
 
-    /**
-     * Prende tutte le note salvate nel db
-     * @param res 
-     * @returns 
-     */
-    @Get('/all')
-    async getAllNotes(@Res() res: any){
-        const notes = await this.notesService.getAllNotes();
-        return notes ? res.status(200).json(notes) : res.status(200).send("Errore nella ricerca di tutte le note");
+  /**
+   * API per recuperare le note associate all'utente loggato
+   * @param req utente loggato
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  @Get()
+  async getAllNotesByUser(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+  ) {
+    try {
+      const notes = await this.notesService.getAllNotesByUser(req.user.id);
+      if (!notes || notes.length === 0)
+        return res.status(404).json({ message: 'Nessuna nota trovata' });
+      return res.status(200).json(notes);
+    } catch (error) {
+      console.error('Errore nel recupero delle note:', error.message);
+      res
+        .status(500)
+        .json({ message: 'Errore nel recupero delle note ' + error.message });
     }
-    
-    /*OPERAZIONI CRUD */
-    /**
-     * Crea una nuova nota
-     * @param noteDto - The data to create a new note
-     * @returns The created note entity
-     */
-    @Post('/create')
-    async create(@Body() noteDto: NoteDto, @Res() res: any) {
-        return res.json(this.notesService.createNote(noteDto));
+  }
+
+  /**
+   * API per prendere tutte le note salvate nel db solo admin
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin)
+  @Get('admin')
+  async getAllNotes(@Res() res: Response) {
+    try {
+      const notes = await this.notesService.getAllNotes();
+      if (!notes || notes.length === 0)
+        return res.status(404).json({ message: 'Nessuna nota trovata' });
+      return res.status(200).json(notes);
+    } catch (error) {
+      console.error('Errore nel recupero delle note:', error.message);
+      res
+        .status(500)
+        .json({ message: 'Errore nel recupero delle note ' + error.message });
     }
+  }
 
-    /**
-     * Aggiorna una nota esistente
-     * @param body corpo della richiesta
-     * @param res 
-     * @returns 
-     */
-    @Post("/update")
-    update(@Body() body: any, @Res() res){
-        const vehicleId = body.vehicleId;
-        const userId = body.userId;
-        const newContent = body.content;
+  /**
+   * API per la creazione di una nuova nota
+   * @param req per recuperare utente e lista veicoli
+   * @param body il veId del veicolo e il contenuto del messaggio
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  @Post()
+  async createNote(
+    @Req() req: Request & { user: UserFromToken },
+    @Body() body: { veId: number; content: string },
+    @Res() res: Response,
+  ) {
+    try {
+      const veId = Number(body.veId); // Garantisce che veId sia un numero
 
-        return res.json(this.notesService.updateNote(userId, vehicleId, newContent));
+      if (isNaN(veId)) {
+        return res
+          .status(400)
+          .json({ message: 'veId deve essere un numero valido' });
+      }
+      await this.notesService.createNote(req.user.id, veId, body.content);
+      return res.status(200).json({ message: 'Nota creata con successo!' });
+    } catch (error) {
+      console.error(
+        'Errore nella registrazione della nuova nota:',
+        error.message,
+      );
+      res.status(500).json({
+        message:
+          'Errore nella registrazione della nuova nota: ' + error.message,
+      });
     }
+  }
 
-    /**
-     * Elimina una nota
-     * @param params id della nota da cancellare
-     * @param res 
-     * @returns 
-     */
-    @Post('/delete/:id')
-    async delete(@Param() params: any, @Res() res){
-        const noteId = params.id;
-        return res.json(this.notesService.deleteNote(noteId));
+  /**
+   * API per aggiornare una nota dato un utente e id della nota
+   * @param req utente loggato
+   * @param noteId id della nota
+   * @param body contenuto da aggiornare
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  @Put(':id')
+  async updateNote(
+    @Req() req: Request & { user: UserFromToken },
+    @Param('id') noteId: number,
+    @Body() body: NoteDto,
+    @Res() res: Response,
+  ) {
+    try {
+      await this.notesService.updateNote(req.user.id, noteId, body.content);
+      return res.status(200).json({ message: 'Nota aggiornata con successo!' });
+    } catch (error) {
+      console.error("Errore nell'aggiornamento della nota:", error.message);
+      res.status(500).json({
+        message: "Errore nell'aggiornamento della nota: " + error.message,
+      });
     }
+  }
 
+  /**
+   * API per eliminare una nota, se utente è admin può eliminare tutte, se user normale può
+   * eliminare solo quelle create da lui
+   * @param req utente loggato
+   * @param noteId id della nota
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  @Delete(':id')
+  @UsePipes(ParseIntPipe)
+  async deleteNote(
+    @Req() req: Request & { user: UserFromToken },
+    @Param('id') noteId: number,
+    @Res() res: Response,
+  ) {
+    try {
+      await this.notesService.deleteNote(req.user.id, noteId);
+      return res.status(200).json({ message: 'Nota eliminata con successo!' });
+    } catch (error) {
+      console.error("Errore nell'eliminazione della nota:", error.message);
+      res.status(500).json({
+        message: "Errore nell'eliminazone della nota: " + error.message,
+      });
+    }
+  }
 }
--- backend_fleet/src/services/notes/notes.service.ts
+++ backend_fleet/src/services/notes/notes.service.ts
@@ -1,174 +1,279 @@
 import { Injectable } from '@nestjs/common';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { NoteDto } from 'classes/dtos/note.dto';
+import { VehicleDTO } from 'classes/dtos/vehicle.dto';
 import { NoteEntity } from 'classes/entities/note.entity';
 import { UserEntity } from 'classes/entities/user.entity';
 import { VehicleEntity } from 'classes/entities/vehicle.entity';
-import { DataSource, Repository } from 'typeorm';
-import { NotFoundException } from '@nestjs/common';  // For better error handling
+import { DataSource, In, Repository } from 'typeorm';
+import { AssociationService } from '../association/association.service';
+import { UserDTO } from 'classes/dtos/user.dto';
 
 @Injectable()
 export class NotesService {
   constructor(
     @InjectRepository(NoteEntity, 'readOnlyConnection')
     private readonly noteRepository: Repository<NoteEntity>,
+    @InjectRepository(UserEntity, 'readOnlyConnection')
+    private readonly userRepository: Repository<UserEntity>,
     @InjectDataSource('mainConnection')
-    private readonly connection: DataSource
+    private readonly connection: DataSource,
+    private readonly associationService: AssociationService,
   ) {}
 
-  async getAllNotes(){
+  /**
+   * Permette di recuperare tutte le note di ogni veicolo di ogni utente, funzione admin
+   * @returns
+   */
+  async getAllNotes() {
     const notes = await this.noteRepository.find({
-      select: {
-        content: true,
-        vehicle: {
-          veId: true
-        }
-      },
       relations: {
-        vehicle: true
-      }
+        vehicle: true,
+        user: true,
+      },
+      order: {
+        vehicle: {
+          plate: 'ASC',
+        },
+      },
     });
-    return notes;
+    return notes.map((note) => this.toDTO(note));
   }
 
-
-  /*OPERAZIONI CRUD */
+  /**
+   * Permette di recuperare tutte le note di ogni veicolo in base all'utente che fa la richiesta
+   * @param userId id utente
+   * @returns
+   */
+  async getAllNotesByUser(userId: number): Promise<any> {
+    const vehicles = (await this.associationService.getVehiclesByUserRole(
+      userId,
+    )) as VehicleEntity[];
+    if (!vehicles || vehicles.length === 0)
+      throw new Error('Nessun Veicolo associato');
+    const vehicleIds = vehicles.map((vehicle) => vehicle.veId);
+    const veIdArray = Array.isArray(vehicleIds) ? vehicleIds : [vehicleIds];
+    const notes = await this.noteRepository.find({
+      relations: {
+        vehicle: true,
+        user: true,
+      },
+      where: {
+        vehicle: {
+          veId: In(veIdArray),
+        },
+        user: {
+          id: userId,
+        },
+      },
+      order: {
+        vehicle: {
+          plate: 'ASC',
+        },
+      },
+    });
+    return notes.map((note) => this.toDTO(note));
+  }
 
   /**
-   * Crea una nuova nota e la immagazzina nel database
-   * @param noteDto - DTO della nota
-   * @returns l'entità "Nota" creata
+   * Permette di creare una nuova nota, associata ad un utente ed un veicolo
+   * @param userId id utente
+   * @param veId veId identificativo veicolo
+   * @param content contenuto della nota
    */
-  async createNote(noteDto: NoteDto){
-    const { content, vehicleId, userId } = noteDto;
-    const queryRunner = this.connection.createQueryRunner();
+  async createNote(userId: number, veId: number, content: string) {
+    if (!content || content.trim().length === 0)
+      throw new Error('Il contenuto della nota non può essere vuoto');
 
+    if (content.length > 500)
+      throw new Error(
+        'Il contenuto della nota supera il limite di 500 caratteri',
+      );
+
+    const user = await this.checkUser(userId);
+
+    const vehicles = (await this.associationService.getVehiclesByUserRole(
+      userId,
+    )) as VehicleEntity[];
+    if (!vehicles || vehicles.length === 0)
+      throw new Error('Nessun Veicolo associato');
+    const vehicle = vehicles.find((v) => v.veId === veId);
+    if (!vehicle) throw new Error('Non hai i permessi per questo veicolo');
+
+    // se già esiste una nota non si puo crearne un altra
+    const existNote = await this.noteRepository.findOne({
+      where: {
+        vehicle: {
+          veId: vehicle.veId,
+        },
+        user: {
+          id: user.id,
+        },
+      },
+    });
+    if (existNote)
+      throw new Error(
+        `Hai già una nota creata per questo veicolo, non puoi crearne un'altra`,
+      );
+
+    const queryRunner = this.connection.createQueryRunner();
     try {
-        // Interruzione dell'esecuzone se almeno uno dei parametri non viene trovato
-        if (!content || !vehicleId || !userId) {
-            return false; 
-        }
-    
-        await queryRunner.startTransaction();
-    
-        const vehicle = await queryRunner.manager.findOne(VehicleEntity, { where: { veId: vehicleId } }); //ricerca del veicolo
-    
-        const user = await queryRunner.manager.findOne(UserEntity, { where: { id: userId } }); //ricerca dell'utente
-    
-        // Interruzione dell'esecuzone se veicolo o utente non trovati
-        if (!vehicle || !user) {
-            console.error('Vehicle or User not found:', { vehicle, user });
-            throw new Error('Invalid vehicleId or userId');
-        }
-    
-        //creazione della nuova nota 
-        const newNote = queryRunner.manager.getRepository(NoteEntity).create({
-            content: content,
-            vehicle: vehicle,  
-            user: user,  
-        });
-        await queryRunner.manager.getRepository(NoteEntity).save(newNote); //Inserimento della nota nel DB
-    
-        await queryRunner.commitTransaction();
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      const newNote = queryRunner.manager.getRepository(NoteEntity).create({
+        content: content,
+        vehicle: vehicle,
+        user: user,
+      });
+      await queryRunner.manager.getRepository(NoteEntity).save(newNote);
+      await queryRunner.commitTransaction();
     } catch (error) {
-        await queryRunner.rollbackTransaction();
-        console.error('Error during transaction:', error);
-        throw error;  
+      console.error('Errore inserimento nuovo utente: ' + error);
+      await queryRunner.rollbackTransaction();
+      throw error;
     } finally {
-        await queryRunner.release();
+      await queryRunner.release();
     }
   }
 
   /**
-   * Modifica il contenuto di una nota
+   * Modifica il contenuto di una nota, un utente può aggiorare soltanto una nota creata da lui,
+   * un admin può modificare una qualsiasi nota
+   * @param userId id utente
    * @param noteId id della nota da modificare
    * @param updatedContent contenuto modificato
    */
   async updateNote(
     userId: number,
-    vehicleId: number,
-    updatedContent: string
+    noteId: number,
+    updatedContent: string,
   ): Promise<void> {
-    const queryRunner = this.connection.createQueryRunner();
-  
-    try {
-      // Interruzione se i parametri non sono validi
-      if (!userId || !vehicleId) {
-        throw new Error('Invalid input parameters');
-      }
-  
-      // Avvia la transazione
-      await queryRunner.startTransaction();
-  
-      // Recupera la nota associata a userId e vehicleId
-      const note = await queryRunner.manager.findOne(NoteEntity, {
-        where: {
-          user: {
-            id: userId,
-          },
-          vehicle: {
-            veId: vehicleId,
-          },
-        },
-      });
-  
-      // Se la nota non esiste, crea una nuova nota
-      if (!note) {
-        const noteDto = new NoteDto();
-        noteDto.userId = userId; // Prendi userId direttamente dalla variabile.
-        noteDto.vehicleId = vehicleId; // Prendi vehicleId direttamente dalla variabile.
-        noteDto.content = updatedContent; // Inizializza con un contenuto vuoto o come preferisci.
+    if (!updatedContent || updatedContent.trim().length === 0)
+      throw new Error('Il contenuto della nota non può essere vuoto');
 
-        this.createNote(noteDto);
-      } else {
-        // Aggiorna la nota
-        await queryRunner.manager.update(
-          NoteEntity,
-          { id: note.id },
-          { content: updatedContent }
+    if (updatedContent.length > 500)
+      throw new Error(
+        'Il contenuto della nota supera il limite di 500 caratteri',
+      );
+    const user = await this.checkUser(userId);
+
+    const note = await this.noteRepository.findOne({
+      where: {
+        id: noteId,
+      },
+      relations: {
+        vehicle: true,
+        user: true,
+      },
+    });
+    if (!note) throw new Error(`Nota con ID ${noteId} non trovata`);
+
+    if (user.role.name !== 'Admin') {
+      const vehicles = (await this.associationService.getVehiclesByUserRole(
+        user.id,
+      )) as VehicleEntity[];
+      if (
+        !vehicles.find((v) => v.veId === note.vehicle.veId) ||
+        note.user.id !== user.id
+      )
+        throw new Error(
+          `Non hai il permesso aggiornare la nota di questo veicolo`,
         );
-      }
-  
-      // Conferma la transazione
+    }
+
+    const queryRunner = this.connection.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      // Aggiorna la nota
+      await queryRunner.manager
+        .getRepository(NoteEntity)
+        .update({ key: note.key }, { content: updatedContent });
       await queryRunner.commitTransaction();
     } catch (error) {
-      // Rollback in caso di errore
+      console.error('Errore durante aggiornamento nota:', error);
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  /**
+   * Elimina una nota dal database, un utente normale può eliminare soltanto una nota creata da lui,
+   * un admin può eliminare una qualisasi nota
+   * @param userId id utente loggato
+   * @param noteId id della nota
+   */
+  async deleteNote(userId: number, noteId: number) {
+    const user = await this.checkUser(userId);
+    const note = await this.noteRepository.findOne({
+      where: {
+        id: noteId,
+      },
+      relations: {
+        vehicle: true,
+        user: true,
+      },
+    });
+    if (!note) throw new Error(`Nota con ID ${noteId} non trovata`);
+    if (user.role.name !== 'Admin') {
+      const vehicles = (await this.associationService.getVehiclesByUserRole(
+        user.id,
+      )) as VehicleEntity[];
+      if (
+        !vehicles.find((v) => v.veId === note.vehicle.veId) ||
+        note.user.id !== user.id
+      )
+        throw new Error(
+          `Non hai il permesso per rimuovere la nota di questo veicolo`,
+        );
+    }
+    const queryRunner = this.connection.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      await queryRunner.manager.getRepository(NoteEntity).remove(note);
+      await queryRunner.commitTransaction();
+    } catch (error) {
       await queryRunner.rollbackTransaction();
       console.error('Error during transaction:', error);
       throw error;
     } finally {
-      // Rilascia il query runner
       await queryRunner.release();
     }
   }
-  
-  
 
   /**
-   * Elimina una nota
-   * @param noteId id della nota da eliminare
+   * Formatta il ritorno
    */
-  async deleteNote(noteId: number){
-    const queryRunner = this.connection.createQueryRunner();
-    try{
-        queryRunner.startTransaction();
-
-        //interruzione dell'esecuzione se parametro non trovato
-        if(!noteId){
-            throw new Error('Invalid note id');
-        }
-
-        const note = await queryRunner.manager.findOne(NoteEntity, { where: { id: noteId }}); //ricerca della nota in base all'id
-
-        queryRunner.manager.remove(note); //rimozione della nota
-
-        await queryRunner.commitTransaction();
-    }catch(error){
-        await queryRunner.rollbackTransaction();
-        console.error('Error during transaction:', error);
-        throw error;  
-    }finally{
-        queryRunner.release();
+  private toDTO(note: NoteEntity) {
+    const noteDTO = new NoteDto();
+    noteDTO.id = note.id;
+    noteDTO.content = note.content;
+    const vehicleDTO = new VehicleDTO();
+    vehicleDTO.id = note.vehicle.id;
+    vehicleDTO.veId = note.vehicle.veId;
+    const userDTO = new UserDTO();
+    if (note.user) {
+      userDTO.id = note.user.id;
+      userDTO.username = note.user.username;
     }
+
+    return {
+      ...noteDTO,
+      vehicle: vehicleDTO,
+      user: userDTO,
+    };
+  }
+
+  private async checkUser(userId: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['role'],
+    });
+    if (!user) throw new Error(`Utente con ID ${userId} non trovato`);
+    return user;
   }
 }
