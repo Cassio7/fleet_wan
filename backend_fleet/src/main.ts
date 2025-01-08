318f54a1b02dee0204a941a839446900f235b7eb
AlessioC <AlessioC@nomail.com>
2025-01-08 16:05:36 +0000

    enable logger

--- backend_fleet/src/main.ts
+++ backend_fleet/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+  const port = 3001;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('v0');
-  await app.listen(3001);
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
