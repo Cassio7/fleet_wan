eccafcdd8923e925467d586c27241d709c7362b7
Lorenzo <lorenzo@nomail.com>
2025-01-16 12:56:54 +0000

    note snackbar component

--- frontend_fleet/src/app/Mezzi/Components/note-snackbar/note-snackbar.component.ts
+++ frontend_fleet/src/app/Mezzi/Components/note-snackbar/note-snackbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, Inject, Input } from '@angular/core';
+import { MAT_SNACK_BAR_DATA, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-note-snackbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSnackBarModule
+  ],
   templateUrl: './note-snackbar.component.html',
   styleUrl: './note-snackbar.component.css'
 })
-export class NoteSnackbarComponent {
+export class NoteSnackbarComponent implements AfterViewChecked{
+  innerText: string = "";
 
+  constructor(private cd: ChangeDetectorRef, @Inject(MAT_SNACK_BAR_DATA) public data: any) {}
+
+  ngAfterViewChecked(): void {
+    this.innerText = this.data.content;
+    this.cd.detectChanges();
+  }
 }
