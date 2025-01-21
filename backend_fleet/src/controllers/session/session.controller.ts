b910cec44796a4ec991ae520e45a7bc46db064a1
AlessioC <AlessioC@nomail.com>
2025-01-21 15:17:42 +0000

    logger, handle errors, fix function return and optimaze functions

--- backend_fleet/src/controllers/session/session.controller.ts
+++ backend_fleet/src/controllers/session/session.controller.ts
@@ -8,14 +8,14 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { VehicleEntity } from 'classes/entities/vehicle.entity';
 import { Role } from 'classes/enum/role.enum';
 import { UserFromToken } from 'classes/interfaces/userToken.interface';
 import { Response } from 'express';
 import { Roles } from 'src/decorators/roles.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RolesGuard } from 'src/guard/roles.guard';
-import { AssociationService } from 'src/services/association/association.service';
+import { LogContext } from 'src/log/logger.types';
+import { LoggerService } from 'src/log/service/logger.service';
 import { SessionService } from 'src/services/session/session.service';
 import { VehicleService } from 'src/services/vehicle/vehicle.service';
 import { validateDateRange } from 'src/utils/utils';
@@ -26,65 +26,105 @@ import { validateDateRange } from 'src/utils/utils';
 export class SessionController {
   constructor(
     private readonly sessionService: SessionService,
-    private readonly associationService: AssociationService,
     private readonly vehicleService: VehicleService,
+    private readonly loggerService: LoggerService,
   ) {}
 
   /**
    * API che restituisce tutte le sessioni attive se la fine è maggiore dell'ultima sessione, quindi veicolo in movimento.
    * @param res
    */
-  @Get('active')
-  async getAllActiveSession(@Res() res: Response) {
-    try {
-      const actives = await this.sessionService.getAllActiveSession();
-      if (actives) {
-        const realActive = [];
-        for (const active of actives) {
-          const last = await this.sessionService.getLastSession(
-            active.vehicle_veId,
-          );
-          if (last) {
-            const firstDate = new Date(active.session_period_to);
-            const secondDate = new Date(last.period_to);
-            if (firstDate >= secondDate) {
-              realActive.push(active);
-            }
-          }
-        }
-        res.status(200).json({
-          sessions: realActive,
-        });
-      } else {
-        res.status(200).json({ message: 'No sessioni attive' });
-      }
-    } catch (error) {
-      console.error('Errore nella ricerca delle sessioni attive: ' + error);
-      res.status(500).json({
-        message: 'Errore nella ricerca delle sessioni attive.',
-      });
-    }
-  }
+  //@Get('active')
+  // async getAllActiveSession(@Res() res: Response) {
+  //   try {
+  //     const actives = await this.sessionService.getAllActiveSession();
+  //     if (actives) {
+  //       const realActive = [];
+  //       for (const active of actives) {
+  //         const last = await this.sessionService.getLastSession(
+  //           active.vehicle_veId,
+  //         );
+  //         if (last) {
+  //           const firstDate = new Date(active.session_period_to);
+  //           const secondDate = new Date(last.period_to);
+  //           if (firstDate >= secondDate) {
+  //             realActive.push(active);
+  //           }
+  //         }
+  //       }
+  //       res.status(200).json({
+  //         sessions: realActive,
+  //       });
+  //     } else {
+  //       res.status(200).json({ message: 'No sessioni attive' });
+  //     }
+  //   } catch (error) {
+  //     console.error('Errore nella ricerca delle sessioni attive: ' + error);
+  //     res.status(500).json({
+  //       message: 'Errore nella ricerca delle sessioni attive.',
+  //     });
+  //   }
+  // }
 
   /**
    * API per prendere tutte le sessioni in base all'id
    * @param res
    * @param params
    */
-  @Get(':id')
-  async getAllSessionByVeId(@Res() res: Response, @Param() params: any) {
+  @Post('veId')
+  async getAllSessionByVeId(
+    @Res() res: Response,
+    @Body() body: { veId: number },
+    @Req() req: Request & { user: UserFromToken },
+  ) {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Session',
+      resourceId: body.veId,
+    };
+    const veId = Number(body.veId); // Garantisce che veId sia un numero
+
+    if (isNaN(veId)) {
+      this.loggerService.logCrudError({
+        error: new Error('Il veId deve essere un numero valido'),
+        context,
+        operation: 'list',
+      });
+      return res.status(400).json({
+        message: 'Il veId deve essere un numero valido',
+      });
+    }
     try {
-      const data = await this.sessionService.getAllSessionByVeId(params.id);
-      if (data.length > 0) {
-        res.status(200).json(data);
-      } else
-        res.status(404).json({ message: `No Session per id: ${params.id}` });
-    } catch (error) {
-      console.error(
-        'Errore nella ricerca della sessione del veicolo: ' + error,
+      const data = await this.sessionService.getAllSessionByVeId(
+        req.user.id,
+        veId,
       );
-      res.status(500).json({
-        message: 'Errore nella ricerca della sessione del veicolo.',
+
+      if (!data?.length) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          'Nessuna sessione trovata',
+        );
+        return res.status(404).json({ message: 'Nessuna sessione trovata' });
+      }
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Lista sessioni (${data.length}) recuperata con successo, VeId = ${veId}`,
+      );
+      return res.status(200).json(data);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'list',
+      });
+
+      return res.status(error.status || 500).json({
+        message:
+          error.message || 'Errore nel recupero delle sessioni del veicolo ',
       });
     }
   }
@@ -92,30 +132,32 @@ export class SessionController {
   /**
    * API per prendere tutte le sessioni indicando range temporale in base all'id
    * @param res
-   * @param params VeId
-   * @param body Data inizio e data fine ricerca
+   * @param body veId del veicolo, Data inizio e data fine ricerca
+   * @param req
    * @returns
    */
-  @Post(':veId')
+  @Post()
   async getAllSessionByVeIdRanged(
     @Res() res: Response,
-    @Param() params: any,
-    @Body() body: any,
+    @Body() body: { veId: number; dateFrom: string; dateTo: string },
     @Req() req: Request & { user: UserFromToken },
   ) {
-    const vehicles = (await this.associationService.getVehiclesByUserRole(
-      req.user.id,
-    )) as VehicleEntity[];
-    if (!vehicles || !Array.isArray(vehicles)) {
-      return res.status(404).json({ message: 'Nessun Veicolo associato' });
-    }
-    const vehicle = vehicles.find(
-      (element) => element.veId === Number(params.veId),
-    );
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Session',
+      resourceId: body.veId,
+    };
+    const veId = Number(body.veId); // Garantisce che veId sia un numero
 
-    if (!vehicle) {
-      return res.status(404).json({
-        message: `Non hai i permessi per visualizzare il veicolo con VeId: ${params.veId}`,
+    if (isNaN(veId)) {
+      this.loggerService.logCrudError({
+        error: new Error('Il veId deve essere un numero valido'),
+        context,
+        operation: 'list',
+      });
+      return res.status(400).json({
+        message: 'Il veId deve essere un numero valido',
       });
     }
     const dateFrom = body.dateFrom;
@@ -124,138 +166,171 @@ export class SessionController {
     // controllo data valida
     const validation = validateDateRange(dateFrom, dateTo);
     if (!validation.isValid) {
-      return res.status(400).json(validation.message);
+      return res.status(400).json({ message: validation.message });
     }
     const dateFrom_new = new Date(dateFrom);
     const dateTo_new = new Date(dateTo);
     try {
-      const data = await this.sessionService.getAllSessionByVeIdRanged(
-        params.veId,
+      const data = await this.sessionService.getAllSessionsByVeIdAndRange(
+        req.user.id,
+        veId,
         dateFrom_new,
         dateTo_new,
       );
-      if (data.length > 0) {
-        res.status(200).json(data);
-      } else
-        res.status(404).json({ message: `No Session per id: ${params.veId}` });
-    } catch (error) {
-      console.error(
-        'Errore nel recupero delle sessioni con range temporale: ' + error,
+
+      if (!data?.length) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          'Nessuna sessione trovata',
+        );
+        return res.status(404).json({ message: 'Nessuna sessione trovata' });
+      }
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Lista sessioni (${data.length}) recuperata con successo, VeId = ${veId}, dateFrom = ${dateFrom_new.toISOString()}, dateFrom = ${dateTo_new.toISOString()}`,
       );
-      res.status(500).json({
-        message: 'Errore nel recupero delle sessioni con range temporale',
+      return res.status(200).json(data);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'list',
+      });
+
+      return res.status(error.status || 500).json({
+        message:
+          error.message ||
+          'Errore nel recupero delle sessioni con range temporale',
       });
     }
   }
 
   /**
-   * Restituisce l'ultima sessione di ogni veicolo in un range di tempo
+   * API per prendere l'ultima sessione in base all veid passato
+   * @param req user data
    * @param res
-   * @param body dateFrom e dateTo del range
+   * @param body VeId veicolo
+   * @returns
    */
-  @Post('lastranged/all')
-  async getAllVehiclesLastSessionByVeIdRanged(
+  @Post('lastvalid')
+  async getLastSession(
+    @Req() req: Request & { user: UserFromToken },
     @Res() res: Response,
-    @Body() body: any,
+    @Body() body: { veId: number },
   ) {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Session',
+      resourceId: body.veId,
+    };
+    const veId = Number(body.veId); // Garantisce che veId sia un numero
+
+    if (isNaN(veId)) {
+      this.loggerService.logCrudError({
+        error: new Error('Il veId deve essere un numero valido'),
+        context,
+        operation: 'read',
+      });
+      return res.status(400).json({
+        message: 'Il veId deve essere un numero valido',
+      });
+    }
     try {
-      const dateFrom = body.dateFrom;
-      const dateTo = body.dateTo;
-      const sessions =
-        await this.sessionService.getAllVehiclesLastSessionByVeIdRanged(
-          dateFrom,
-          dateTo,
-        );
-      if (sessions) res.status(200).json(sessions);
-      else res.status(404).json({ message: 'No last session found' });
-    } catch (error) {
-      console.error(
-        'Errore nella ricerca della sessione del veicolo: ' + error,
+      const data = await this.sessionService.getLastSession(
+        req.user.id,
+        body.veId,
       );
-      res.status(500).json({
-        message: 'Errore nella ricerca della sessione del veicolo.',
+      if (!data) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'read',
+          'Ultima sessione non trovata',
+        );
+        return res.status(404).json({ message: 'Ultima sessione non trovata' });
+      }
+      0;
+      this.loggerService.logCrudSuccess(
+        context,
+        'read',
+        `Ultima sessione recuperata con successo, VeId = ${veId}, Sequence_id = ${data.sequence_id}`,
+      );
+      return res.status(200).json(data);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'read',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore nel recupero ultima sessione',
       });
     }
   }
+
   /**
    * Ritorna un array con l'ultima sessione di tutti i veicoli
    * @param res
    */
-  @Get('last/all')
-  async getAllVehiclesLastSession(@Res() res: Response) {
-    try {
-      const vehicles = await this.vehicleService.getAllVehicles(); // Prendere tutti i veicoli
-      const lastSessions = await Promise.all(
-        vehicles.map(async (vehicle) => {
-          return this.sessionService.getLastSession(vehicle.veId); // Per ogni veicolo, cercare l'ultima sessione
-        }),
-      );
-      if (lastSessions.length > 0)
-        res.status(200).json(lastSessions); // Restituire l'array di sessioni come JSON
-      else res.status(404).json({ message: 'Nessuna sessione trovata' });
-    } catch (error) {
-      console.error(
-        "Errore nella ricerca dell'ultima sessione del veicolo: " + error,
-      );
-      res.status(500).json({
-        message: "Errore nella ricerca dell'ultima sessione del veicolo.",
-      });
-    }
-  }
-
-  /**
-   * API per prendere l'ultima sessione in base all'id
-   * @param res
-   * @param params VeId identificativo Veicolo
-   */
-  @Get('last/:id')
-  async getLastSession(@Res() res: Response, @Param() params: any) {
-    try {
-      const data = await this.sessionService.getLastSession(params.id);
-      if (data) res.status(200).json(data);
-      else res.status(404).json({ message: `No Session per id: ${params.id}` });
-    } catch (error) {
-      console.error("Errore nel recupero dell'ultima sessione: " + error);
-      res.status(500).json({
-        message: "Errore nel recupero dell'ultima sessione",
-      });
-    }
-  }
+  //@Get('last/all')
+  // async getAllVehiclesLastSession(@Res() res: Response) {
+  //   try {
+  //     const vehicles = await this.vehicleService.getAllVehicles(); // Prendere tutti i veicoli
+  //     const lastSessions = await Promise.all(
+  //       vehicles.map(async (vehicle) => {
+  //         return this.sessionService.getLastSession(vehicle.veId); // Per ogni veicolo, cercare l'ultima sessione
+  //       }),
+  //     );
+  //     if (lastSessions.length > 0)
+  //       res.status(200).json(lastSessions); // Restituire l'array di sessioni come JSON
+  //     else res.status(404).json({ message: 'Nessuna sessione trovata' });
+  //   } catch (error) {
+  //     console.error(
+  //       "Errore nella ricerca dell'ultima sessione del veicolo: " + error,
+  //     );
+  //     res.status(500).json({
+  //       message: "Errore nella ricerca dell'ultima sessione del veicolo.",
+  //     });
+  //   }
+  // }
 
   /**
    * API che restituisce la sessione attiva se, la fine è maggiore dell'ultima sessione, quindi veicolo in movimento.
    * @param res
    * @param params VeId identificativo Veicolo
    */
-  @Get('active/:id')
-  async getActiveSessionByVeId(@Res() res: Response, @Param() params: any) {
-    try {
-      const active = await this.sessionService.getActiveSessionByVeId(
-        params.id,
-      );
-      const last = await this.sessionService.getLastSession(params.id);
-      if (!active || !last) {
-        res.status(404).json({
-          message: `Nessuna sessione attiva registrata per id: ${params.id}`,
-        });
-      } else {
-        const firstDate = new Date(active.period_to);
-        const secondDate = new Date(last.period_to);
-        if (firstDate > secondDate) {
-          res.status(200).json({
-            session: active,
-          });
-        } else {
-          res.status(200).json({ message: 'Non attivo' });
-        }
-      }
-    } catch (error) {
-      console.error('Errore nel recupero della sessione attiva: ' + error);
-      res.status(500).json({
-        message: 'Errore nel recupero della sessione attiva',
-      });
-    }
-  }
+  //@Get('active/:id')
+  // async getActiveSessionByVeId(@Res() res: Response, @Param() params: any) {
+  //   try {
+  //     const active = await this.sessionService.getActiveSessionByVeId(
+  //       params.id,
+  //     );
+  //     const last = await this.sessionService.getLastSession(params.id);
+  //     if (!active || !last) {
+  //       res.status(404).json({
+  //         message: `Nessuna sessione attiva registrata per id: ${params.id}`,
+  //       });
+  //     } else {
+  //       const firstDate = new Date(active.period_to);
+  //       const secondDate = new Date(last.period_to);
+  //       if (firstDate > secondDate) {
+  //         res.status(200).json({
+  //           session: active,
+  //         });
+  //       } else {
+  //         res.status(200).json({ message: 'Non attivo' });
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error('Errore nel recupero della sessione attiva: ' + error);
+  //     res.status(500).json({
+  //       message: 'Errore nel recupero della sessione attiva',
+  //     });
+  //   }
+  // }
 
   /**
    * API per prendere tutte le distanze delle sessioni in base all'id
@@ -290,7 +365,7 @@ export class SessionController {
     // controllo data valida
     const validation = validateDateRange(dateFrom, dateTo);
     if (!validation.isValid) {
-      return res.status(400).json(validation.message);
+      return res.status(400).json({ message: validation.message });
     }
     const dateFrom_new = new Date(dateFrom);
     const dateTo_new = new Date(dateTo);
