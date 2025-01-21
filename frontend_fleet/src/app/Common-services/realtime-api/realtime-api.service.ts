5bac891bab6bd86eca9deafda7a6b2bf4136e8ff
Lorenzo <lorenzo@nomail.com>
2025-01-21 12:39:40 +0000

    realtime api service

---
+++ frontend_fleet/src/app/Common-services/realtime-api/realtime-api.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SessionStorageService } from '../sessionStorage/session-storage.service';
+import { RealtimeData } from '../../Models/RealtimeData';
+import { CommonService } from '../common service/common.service';
+import { Observable } from 'rxjs';
+import { CookiesService } from '../cookies service/cookies.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RealtimeApiService {
+
+  constructor(
+    private http: HttpClient,
+    private commonService: CommonService,
+    private cookieService: CookiesService
+  ) { }
+
+  getLastRealtime(): Observable<RealtimeData[]>{
+    const access_token = this.cookieService.getCookie("user");
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${access_token}`,
+      'Content-Type': 'application/json'
+    });
+    return this.http.get<RealtimeData[]>(`${this.commonService.url}/realtimes/last`, {headers});
+  }
+}
