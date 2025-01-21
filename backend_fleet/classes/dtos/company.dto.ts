139281724163a8e8cb5bcff5b3b397cde26b5402
AlessioC <AlessioC@nomail.com>
2025-01-21 15:13:49 +0000

    no more option check

--- backend_fleet/classes/dtos/company.dto.ts
+++ backend_fleet/classes/dtos/company.dto.ts
@@ -1,9 +1,8 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 import { CommonDTO } from 'classes/common/common.dto';
 export class CompanyDTO extends CommonDTO {
   @IsNumber()
   suId: number;
-  @IsOptional()
   @IsString()
-  name?: string | null;
+  name: string;
 }
