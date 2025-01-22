5bcfe1a008716191074757effc656e7778b6da17
AlessioC <AlessioC@nomail.com>
2025-01-22 10:42:27 +0000

    getNoteByVeId() function

--- backend_fleet/src/controllers/notes/notes.controller.ts
+++ backend_fleet/src/controllers/notes/notes.controller.ts
@@ -52,16 +52,14 @@ export class NotesController {
     try {
       const notes = await this.notesService.getAllNotesByUser(req.user.id);
 
-      // if (!notes?.length) {
-      //   this.loggerService.logCrudSuccess(
-      //     context,
-      //     'list',
-      //     'Nessuna nota trovata',
-      //   );
-      //   return res.status(404).json({
-      //     message: 'Nessuna nota trovata',
-      //   });
-      // }
+      if (!notes?.length) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          'Nessuna nota trovata',
+        );
+        return res.status(204).json();
+      }
 
       this.loggerService.logCrudSuccess(
         context,
@@ -69,6 +67,67 @@ export class NotesController {
         `Recuperate ${notes.length} note`,
       );
       return res.status(200).json(notes);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'list',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore recupero nota',
+      });
+    }
+  }
+
+  /**
+   * API per il recupero della nota associata all'utente loggato ed in base al veid passato nel body
+   * @param req user data
+   * @param body veId identificativo del veicolo
+   * @param res
+   * @returns
+   */
+  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  @Post('veId')
+  async getNoteByVeid(
+    @Req() req: Request & { user: UserFromToken },
+    @Body() body: { veId: number },
+    @Res() res: Response,
+  ) {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Notes',
+      resourceId: body.veId,
+    };
+    const veId = Number(body.veId); // Garantisce che veId sia un numero
+
+    if (isNaN(veId)) {
+      this.loggerService.logCrudError({
+        error: new Error('Il veId deve essere un numero valido'),
+        context,
+        operation: 'list',
+      });
+      return res.status(400).json({
+        message: 'Il veId deve essere un numero valido',
+      });
+    }
+    try {
+      const note = await this.notesService.getNoteByVeId(req.user.id, veId);
+      if (!note) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          `Nessuna nota recuperata`,
+        );
+        return res.status(204).json();
+      }
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Recuperata la nota id: ${note.id}`,
+      );
+      return res.status(200).json(note);
     } catch (error) {
       this.loggerService.logCrudError({
         error,
@@ -108,9 +167,7 @@ export class NotesController {
           'list',
           'Nessuna nota trovata',
         );
-        return res.status(404).json({
-          message: 'Nessuna nota trovata',
-        });
+        return res.status(204).json();
       }
 
       this.loggerService.logCrudSuccess(
