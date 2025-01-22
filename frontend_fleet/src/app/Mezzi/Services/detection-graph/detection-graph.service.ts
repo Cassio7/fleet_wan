38a03c9e34ac132e6869e14d27cc74d8d720e76d
Lorenzo <lorenzo@nomail.com>
2025-01-22 15:42:09 +0000

    getDetectionQualityByVeId()

--- frontend_fleet/src/app/Mezzi/Services/detection-graph/detection-graph.service.ts
+++ frontend_fleet/src/app/Mezzi/Services/detection-graph/detection-graph.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DetectionQuality } from '../../../Models/DetectionQuality';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookiesService } from '../../../Common-services/cookies service/cookies.service';
+import { CommonService } from '../../../Common-services/common service/common.service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +11,29 @@ import { Injectable } from '@angular/core';
 export class DetectionGraphService {
   private _colors: string[] = ["#5C9074"];
 
-  constructor() { }
+  constructor(
+    private commonService: CommonService,
+    private http: HttpClient,
+    private cookiesService: CookiesService
+  ) { }
 
+  /**
+   * Permette di prendere dal database tutte le qualità di lettura registrate da un veicolo
+   * @param veId veId del veicolo di cui ricercare la qualità delle letture
+   * @returns observable http post
+   */
+  getDetectionQualityByVeId(veId: number): Observable<DetectionQuality[]>{
+    const access_token = this.cookiesService.getCookie("user");
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${access_token}`,
+      'Content-Type': 'application/json'
+    });
 
+    const body = {
+      veId: veId
+    };
+    return this.http.post<DetectionQuality[]>(`http://10.1.0.102:3002/v0/tag/detection`, body, {headers});
+  }
 
   public get colors(): string[] {
     return this._colors;
