40bd09767562b6c696ff172050588fb8499d8a12
Lorenzo <lorenzo@nomail.com>
2025-01-23 13:59:08 +0000

    component rename

--- frontend_fleet/src/app/Common-components/note-section/note-section.component.ts
+++ frontend_fleet/src/app/Common-components/note-section/note-section.component.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { skip, Subject, takeUntil } from 'rxjs';
-import { Note } from '../../../../Models/Note';
-import { Vehicle } from '../../../../Models/Vehicle';
-import { NotesService } from '../../../Services/notes/notes.service';
-import { User } from '../../../../Models/User';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { NoteSnackbarComponent } from '../../note-snackbar/note-snackbar.component';
-import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from "@angular/common";
+import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef, Input, inject, ChangeDetectorRef, ViewEncapsulation } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSnackBarModule, MatSnackBar } from "@angular/material/snack-bar";
+import { Subject, takeUntil } from "rxjs";
+import { NoteSnackbarComponent } from "../../Mezzi/Components/note-snackbar/note-snackbar.component";
+import { NotesService } from "../../Mezzi/Services/notes/notes.service";
+import { Note } from "../../Models/Note";
+import { Vehicle } from "../../Models/Vehicle";
+import { User } from "../../Models/User";
 
 @Component({
   selector: 'app-note-section',
@@ -17,10 +17,10 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatSnackBarModule
-  ],
+    MatSnackBarModule,
+],
   templateUrl: './note-section.component.html',
-  styleUrl: './note-section.component.css'
+  styleUrl: './note-section.component.css',
 })
 export class NoteSectionComponent implements AfterViewInit, OnDestroy{
   private readonly destroy$: Subject<void> = new Subject<void>();
