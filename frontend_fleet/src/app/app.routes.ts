73949e8ba607da779b148685cf50a3cf6b95ae76
Lorenzo <lorenzo@nomail.com>
2025-01-30 12:55:33 +0000

    marginal storico mezzi implementations

--- frontend_fleet/src/app/app.routes.ts
+++ frontend_fleet/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SignUpComponent } from './Common-components/sign-up/sign-up.component';
 import { DashboardComponent } from './Dashboard/Components/dashboard/dashboard.component';
 import { HomeMezziComponent } from './Mezzi/Components/home-mezzi/home-mezzi.component';
 import { DettaglioMezzoComponent } from './Common-components/Scheda-mezzo/dettaglio-mezzo/dettaglio-mezzo.component';
+import { StoricoMezziComponent } from './Storico-mezzi/storico-mezzi/storico-mezzi.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'sign-up', component: SignUpComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'sign-up', component: SignUpComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'home-mezzi', component: HomeMezziComponent },
-    {path:'dettaglio-mezzo/:id', component:DettaglioMezzoComponent},
-    {path: '', component: DashboardComponent},
+    { path:'dettaglio-mezzo/:id', component:DettaglioMezzoComponent },
+    { path: 'storico-mezzi', component:StoricoMezziComponent },
+    { path: '', component: DashboardComponent },
 ];
