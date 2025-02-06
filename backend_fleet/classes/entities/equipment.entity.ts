cf059e4755aa6c8c20894fdc4cfa6a79b69d4adb
AlessioC <AlessioC@nomail.com>
2025-02-06 09:10:36 +0000

    new Equipment entity for vehicle

---
+++ backend_fleet/classes/entities/equipment.entity.ts
@@ -0,0 +1,19 @@
+import { CommonEntity } from 'classes/common/common.entity';
+import { EquipmentInterface } from './../interfaces/equipment.interface';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { VehicleEntity } from './vehicle.entity';
+
+@Entity({
+  name: 'equipments',
+  comment: `Indica le tipologie di attrezzature installate`,
+})
+export class EquipmentEntity
+  extends CommonEntity
+  implements EquipmentInterface
+{
+  @Column()
+  name: string;
+
+  @OneToMany(() => VehicleEntity, (vehicle) => vehicle.equipment)
+  vehicle: VehicleEntity[];
+}
---
+++ backend_fleet/classes/interfaces/equipment.interface.ts
@@ -0,0 +1,5 @@
+import { CommonInterface } from 'classes/common/common.interface';
+
+export interface EquipmentInterface extends CommonInterface {
+  name: string;
+}
