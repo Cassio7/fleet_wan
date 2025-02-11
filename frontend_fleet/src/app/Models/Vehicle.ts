66582c6f83bbb650f59400f840e34ba11c9ecbed
AlessioC <AlessioC@nomail.com>
2025-02-11 12:22:10 +0000

    style

--- frontend_fleet/src/app/Models/Vehicle.ts
+++ frontend_fleet/src/app/Models/Vehicle.ts
@@ -1,18 +1,20 @@
-import { Company } from "./Company";
-import { Device } from "./Device";
-import { Equipment } from "./Equipment";
-import { Group } from "./Group";
-import { Note } from "./Note";
-import { Realtime } from "./Realtime";
-import { Service } from "./Service";
-import { WorkSite } from "./Worksite";
-import { Workzone } from "./Workzone";
+import { Company } from './Company';
+import { Device } from './Device';
+import { Equipment } from './Equipment';
+import { Group } from './Group';
+import { Note } from './Note';
+import { Realtime } from './Realtime';
+import { Rental } from './Rental';
+import { Service } from './Service';
+import { WorkSite } from './Worksite';
+import { Workzone } from './Workzone';
 
 export class Vehicle {
   constructor(
     public id: number,
     public plate: string,
     public model: string,
+    public model_csv: string | null,
     public veId: number,
     public isRFIDReader: boolean,
     public allestimento: boolean,
@@ -20,7 +22,11 @@ export class Vehicle {
     public note: Note | null,
     public device: Device,
     public realtime: Realtime,
-    public relevant_company?: string,
+    public retired_event: Date | null,
+    public fleet_number: number | null,
+    public fleet_install: string | null,
+    public registration?: string,
+    public rental?: Rental,
     public worksite?: WorkSite,
     public workzone?: Workzone,
     public group?: Group,
@@ -29,4 +35,3 @@ export class Vehicle {
     public equipment?: Equipment
   ) {}
 }
-
