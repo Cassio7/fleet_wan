1e8101e3f158027b12c042259fd53537e1e9e481
AlessioC <AlessioC@nomail.com>
2025-02-13 15:05:22 +0000

    add marker icon and colors by anomalies

--- frontend_fleet/src/app/Common-components/map/map.component.ts
+++ frontend_fleet/src/app/Common-components/map/map.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnDestroy,
+  ViewEncapsulation,
+} from '@angular/core';
 import * as L from 'leaflet';
 import { MapService } from '../../Common-services/map/map.service';
 import { skip, Subject, takeUntil } from 'rxjs';
@@ -11,13 +18,10 @@ import { RealtimeData } from '../../Models/RealtimeData';
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatFormFieldModule
-  ],
+  imports: [CommonModule, MatFormFieldModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class MapComponent implements AfterViewInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject<void>();
@@ -29,31 +33,35 @@ export class MapComponent implements AfterViewInit, OnDestroy {
   private lat: number = 43.1121;
   private long: number = 12.3888;
 
-  constructor(private mapService: MapService,private cd: ChangeDetectorRef){}
+  constructor(private mapService: MapService, private cd: ChangeDetectorRef) {}
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-
   ngAfterViewInit(): void {
-    this.mapService.loadMap$.pipe(takeUntil(this.destroy$), skip(1))
-    .subscribe({
+    this.mapService.loadMap$.pipe(takeUntil(this.destroy$), skip(1)).subscribe({
       next: (realtimeData: RealtimeData | null) => {
-        if(realtimeData && realtimeData.realtime){
+        if (realtimeData && realtimeData.realtime) {
+          console.log(realtimeData.anomaly);
           this.lat = realtimeData.realtime.latitude;
           this.long = realtimeData.realtime.longitude;
-
+          console.log();
           this.initMap();
-          const marker = this.mapService.createMarker(this.lat,this.long, realtimeData.vehicle.plate);
+          const marker = this.mapService.createMarker(
+            this.lat,
+            this.long,
+            realtimeData.vehicle.plate,
+            realtimeData.anomaly
+          );
           this.mapService.addMarker(this.map, marker);
         }
-      }
+      },
     });
   }
 
-  private initMap(){
+  private initMap() {
     this.initialized = true;
     this.cd.detectChanges();
     this.mapService.removeMap(this.map);
--- frontend_fleet/src/app/Common-services/map/map.service.ts
+++ frontend_fleet/src/app/Common-services/map/map.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import L from 'leaflet';
 import { BehaviorSubject, map, Subject } from 'rxjs';
 import { RealtimeData } from '../../Models/RealtimeData';
+import { Anomaly } from '../../Models/Anomaly';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MapService {
-  private readonly _loadMap$: BehaviorSubject<RealtimeData|null> = new BehaviorSubject<RealtimeData|null>(null);
+  private readonly _loadMap$: BehaviorSubject<RealtimeData | null> =
+    new BehaviorSubject<RealtimeData | null>(null);
 
-  constructor() { }
+  constructor() {}
 
   /**
    * Inizializza una mappa
@@ -19,14 +21,18 @@ export class MapService {
   initMap(map: L.Map, lat: number, long: number): L.Map {
     map = L.map('map', {
       center: [lat, long],
-      zoom: 12
+      zoom: 12,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
 
     tiles.addTo(map);
     return map;
@@ -36,8 +42,8 @@ export class MapService {
    * rimuove una mappa, se esiste
    * @param map mappa da rimuovere
    */
-  removeMap(map: L.Map){
-    if(map){
+  removeMap(map: L.Map) {
+    if (map) {
       map.remove();
     }
   }
@@ -46,7 +52,7 @@ export class MapService {
    * Aggiunge un marker alla mappa
    * @param marker marker da aggiugere
    */
-  addMarker(map: L.Map, marker: L.Marker<any>){
+  addMarker(map: L.Map, marker: L.Marker<any>) {
     marker.addTo(map);
   }
 
@@ -57,23 +63,109 @@ export class MapService {
    * @param msg contenuto del messaggio che appare onMouseOver
    * @returns marker Lea Flet
    */
-  createMarker(lat: number, long: number, msg: string): L.Marker<any>{
-    const marker = L.marker([lat, long]); //creazione del marker
-    marker.bindPopup(msg, { autoClose: false });
+  createMarker(
+    lat: number,
+    long: number,
+    msg: string,
+    anomaly: Anomaly | undefined
+  ): L.Marker<any> {
+    let customIcon = L.divIcon({
+      className: 'custom-div-icon',
+      html: `<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g filter="url(#filter0_d_206_1354)">
+          <circle cx="16.8809" cy="12.0039" r="12" fill="#5C9074"/>
+          <circle cx="16.8809" cy="12.0039" r="11" stroke="white" stroke-width="2"/>
+          </g>
+          <defs>
+          <filter id="filter0_d_206_1354" x="0.880859" y="0.00390625" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_206_1354"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_206_1354" result="shape"/>
+          </filter>
+          </defs>
+          </svg>
+`,
+      popupAnchor: [11, -6],
+    });
+    if (anomaly) {
+      if (anomaly.gps || anomaly.antenna || anomaly.session)
+        customIcon = L.divIcon({
+          className: 'custom-div-icon',
+          html: `<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g filter="url(#filter0_d_206_1349)">
+          <circle cx="16.8809" cy="12.4832" r="12" fill="#D02626"/>
+          <circle cx="16.8809" cy="12.4832" r="11" stroke="white" stroke-width="2"/>
+          </g>
+          <defs>
+          <filter id="filter0_d_206_1349" x="0.880859" y="0.483154" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_206_1349"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_206_1349" result="shape"/>
+          </filter>
+          </defs>
+          </svg>
+      `,
+          popupAnchor: [11, -6],
+        });
 
-    //Apre il popup quando il marker viene aggiunto
-    marker.on('add', () => {
+      if (
+        anomaly.gps &&
+        !anomaly.gps.includes('totale') &&
+        !anomaly.antenna &&
+        !anomaly.session
+      )
+        customIcon = L.divIcon({
+          className: 'custom-div-icon',
+          html: `<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_206_1349)">
+        <circle cx="16.8809" cy="12.4832" r="12" fill="#C5D026"/>
+        <circle cx="16.8809" cy="12.4832" r="11" stroke="white" stroke-width="2"/>
+        </g>
+        <defs>
+        <filter id="filter0_d_206_1349" x="0.880859" y="0.483154" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_206_1349"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_206_1349" result="shape"/>
+        </filter>
+        </defs>
+        </svg>
+    `,
+          popupAnchor: [11, -6],
+        });
+    }
+
+    const marker = L.marker([lat, long], { icon: customIcon }); //creazione del marker
+    marker.bindPopup(msg, { autoClose: true });
+
+    // Apre il popup solo quando si passa sopra al marker
+    marker.on('mouseover', () => {
       marker.openPopup();
     });
-    //Apre il popup quando si passa sopra al marker
-    marker.on('mouseover', () =>{
-      marker.openPopup();
+
+    // Chiude il popup quando il mouse esce dal marker
+    marker.on('mouseout', () => {
+      marker.closePopup();
     });
 
     return marker;
   }
 
-  public get loadMap$(): BehaviorSubject<RealtimeData|null> {
+  public get loadMap$(): BehaviorSubject<RealtimeData | null> {
     return this._loadMap$;
   }
 }
