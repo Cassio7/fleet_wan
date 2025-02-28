7b4d208e4179df28b7b7723f70440e3904ea21f5
Lorenzo <lorenzo@nomail.com>
2025-02-28 12:56:08 +0000

    storico mezzo basic path type switch style

--- frontend_fleet/src/app/Storico-mezzi/Components/storico-mezzi/storico-mezzi.component.ts
+++ frontend_fleet/src/app/Storico-mezzi/Components/storico-mezzi/storico-mezzi.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MapComponent } from "../../../Common-components/map/map.component";
 import { SessionHystoriesComponent } from "../../../Common-components/Scheda-mezzo/session-hystories/session-hystories.component";
 import { SessionTableComponent } from "../../../Common-components/Scheda-mezzo/session-table/session-table.component";
 import { ListaMezziComponent } from "../lista-mezzi/lista-mezzi.component";
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SessionApiService } from '../../../Common-services/session/session-api.service';
 import { SessionFiltersComponent } from "../../../Common-components/Scheda-mezzo/session-filters/session-filters.component";
 import { ListaFiltersComponent } from "../lista-filters/lista-filters.component";
@@ -11,32 +11,65 @@ import { Vehicle } from '../../../Models/Vehicle';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MapService } from '../../../Common-services/map/map.service';
+import { skip, Subject, takeUntil } from 'rxjs';
+import { RealtimeData } from '../../../Models/RealtimeData';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-storico-mezzi',
   standalone: true,
   imports: [
+    CommonModule,
     MapComponent,
     SessionHystoriesComponent,
     ListaMezziComponent,
     MatListModule,
     MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatIconModule,
+    MatSlideToggleModule,
     SessionFiltersComponent,
     ListaFiltersComponent
 ],
   templateUrl: './storico-mezzi.component.html',
   styleUrl: './storico-mezzi.component.css'
 })
-export class StoricoMezziComponent{
+export class StoricoMezziComponent implements AfterViewInit{
+  private readonly destroy$: Subject<void> = new Subject<void>();
   private _selectedVehicle!: Vehicle;
 
-  constructor(private cd: ChangeDetectorRef){}
+  linedPath: boolean = false;
+  isMapLoaded: boolean = false;
+
+  constructor(
+    private mapService: MapService,
+    private cd: ChangeDetectorRef
+  ){}
+
+  ngAfterViewInit(): void {
+    this.handleMapInit();
+  }
+
+  private handleMapInit(){
+    this.mapService.initMap$.pipe(takeUntil(this.destroy$), skip(1))
+    .subscribe({
+      next: () => {
+        this.isMapLoaded = true;
+      },
+    });
+  }
 
   onVehicleSelection(vehicle: Vehicle){
     this.selectedVehicle = vehicle;
     this.cd.detectChanges();
     console.log("veicolo arrivato a padre: ", vehicle);
+  }
+
+  togglePathType(){
+
   }
 
   public get selectedVehicle(): Vehicle {
