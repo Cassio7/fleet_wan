c092f49956452e87286ccf5374f4809583596c15
AlessioC <AlessioC@nomail.com>
2025-03-03 10:24:13 +0000

    fix session show on different days

--- frontend_fleet/src/app/Common-components/Scheda-mezzo/session-table/session-table.component.ts
+++ frontend_fleet/src/app/Common-components/Scheda-mezzo/session-table/session-table.component.ts
@@ -1,21 +1,39 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { MatButton, MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { SessionApiService } from '../../../Common-services/session/session-api.service';
-import { catchError, last, Observable, of, skip, Subject, takeUntil } from 'rxjs';
-import { VehicleAnomalies } from '../../../Models/VehicleAnomalies';
-import { Vehicle } from '../../../Models/Vehicle';
-import { Anomaly } from '../../../Models/Anomaly';
-import { CheckErrorsService } from '../../../Common-services/check-errors/check-errors.service';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { Session } from '../../../Models/Session';
-import { Point } from '../../../Models/Point';
-import { MapService } from '../../../Common-services/map/map.service';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router } from '@angular/router';
+import { catchError, Observable, of, skip, Subject, takeUntil } from 'rxjs';
+import { CheckErrorsService } from '../../../Common-services/check-errors/check-errors.service';
+import { MapService } from '../../../Common-services/map/map.service';
+import { SessionApiService } from '../../../Common-services/session/session-api.service';
+import { Anomaly } from '../../../Models/Anomaly';
+import { Point } from '../../../Models/Point';
+import { Session } from '../../../Models/Session';
+import { Vehicle } from '../../../Models/Vehicle';
+import { VehicleAnomalies } from '../../../Models/VehicleAnomalies';
 
 @Component({
   selector: 'app-session-table',
@@ -26,15 +44,21 @@ import { Router } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
-    MatFormFieldModule
+    MatFormFieldModule,
   ],
   templateUrl: './session-table.component.html',
   styleUrl: './session-table.component.css',
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', visibility: 'hidden'})),
-      state('expanded', style({height: '*', visibility: 'visible'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', visibility: 'hidden' })
+      ),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
     ]),
   ],
 })
@@ -50,8 +74,22 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
   allSessions: Session[] = [];
 
   // Define ALL possible columns
-  allDaysColumns: string[] = ['expand', 'Data', 'Stato GPS', 'Stato Antenna', 'Sessione', 'Map'];
-  allSessionColumns: string[] = ['Id', 'Sequence ID', 'Inizio', 'Fine', 'Distanza', 'Map'];
+  allDaysColumns: string[] = [
+    'expand',
+    'Data',
+    'Stato GPS',
+    'Stato Antenna',
+    'Sessione',
+    'Map',
+  ];
+  allSessionColumns: string[] = [
+    'Id',
+    'Sequence ID',
+    'Inizio',
+    'Fine',
+    'Distanza',
+    'Map',
+  ];
 
   // These are the displayed columns - they will be updated
   displayedDaysColumns: string[] = [];
@@ -85,39 +123,46 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
 
     //caricamento dati tramite sottoscrizione a cambiamenti nel range di date
     this.sessionApiService.loadAnomalySessionDays$
-    .pipe(takeUntil(this.destroy$), skip(1))
-    .subscribe({
-      next: (range: Date[]) => {
-        if(range){
-          this.lastDateFrom = range[0];
-          this.lastDateTo = range[1];
-          this.fillTable(this.lastDateFrom, this.lastDateTo);
-        }
-      },
-      error: error => console.error("Errore nella notifica per il caricamento delle anomalie per giornata: ", error)
-    });
+      .pipe(takeUntil(this.destroy$), skip(1))
+      .subscribe({
+        next: (range: Date[]) => {
+          if (range) {
+            this.lastDateFrom = range[0];
+            this.lastDateTo = range[1];
+            this.fillTable(this.lastDateFrom, this.lastDateTo);
+          }
+        },
+        error: (error) =>
+          console.error(
+            'Errore nella notifica per il caricamento delle anomalie per giornata: ',
+            error
+          ),
+      });
   }
 
   private checkDettaglio() {
     const currentRoute = this.router.url;
-    this.isDettaglio = currentRoute.startsWith("/dettaglio-mezzo");
+    this.isDettaglio = currentRoute.startsWith('/dettaglio-mezzo');
 
     if (this.isDettaglio) {
       // Filter the DISPLAYED columns
-      this.displayedDaysColumns = this.allDaysColumns.filter(col => col !== "Map");
-      this.displayedSessionColumns = this.allSessionColumns.filter(col => col !== "Map");
+      this.displayedDaysColumns = this.allDaysColumns.filter(
+        (col) => col !== 'Map'
+      );
+      this.displayedSessionColumns = this.allSessionColumns.filter(
+        (col) => col !== 'Map'
+      );
     } else {
       // Add to DISPLAYED columns if not already there
-      if (!this.displayedDaysColumns.includes("Map")) {
-        this.displayedDaysColumns.push("Map");
+      if (!this.displayedDaysColumns.includes('Map')) {
+        this.displayedDaysColumns.push('Map');
       }
-      if (!this.displayedSessionColumns.includes("Map")) {
-        this.displayedSessionColumns.push("Map");
+      if (!this.displayedSessionColumns.includes('Map')) {
+        this.displayedSessionColumns.push('Map');
       }
     }
     this.cd.detectChanges(); // Absolutely essential!
   }
-
 
   /**
    * Controlla quando avvengono cambiamenti solo nel veicolo preso in input,
@@ -138,7 +183,7 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
    */
   private fillTable(dateFrom: Date, dateTo: Date): void {
     if (!dateFrom || !dateTo || dateTo < dateFrom) {
-      console.warn("Errore: Nessun dato valido ricevuto per il range di date.");
+      console.warn('Errore: Nessun dato valido ricevuto per il range di date.');
       return;
     }
 
@@ -146,11 +191,16 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
     this.lastDateFrom = dateFrom;
     this.lastDateTo = dateTo;
 
-    this.sessionApiService.getDaysAnomaliesRangedByVeid(this.vehicle.veId, this.lastDateFrom, this.lastDateTo)
+    this.sessionApiService
+      .getDaysAnomaliesRangedByVeid(
+        this.vehicle.veId,
+        this.lastDateFrom,
+        this.lastDateTo
+      )
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (vehicleAnomalies: VehicleAnomalies) => {
-          console.log("Anomalie per il range di date: ", vehicleAnomalies);
+          console.log('Anomalie per il range di date: ', vehicleAnomalies);
 
           if (vehicleAnomalies && vehicleAnomalies.anomalies.length > 0) {
             this.data = true;
@@ -163,10 +213,13 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
             this.anomaliesTable.renderRows();
           }
         },
-        error: (error) => console.error("Errore nel caricamento delle anomalie nel range di date: ", error)
+        error: (error) =>
+          console.error(
+            'Errore nel caricamento delle anomalie nel range di date: ',
+            error
+          ),
       });
   }
-
 
   /**
    * Funzione chiamata quando una riga viene espansa,
@@ -179,21 +232,27 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
     this.expandedDay = this.expandedDay === anomaly ? null : anomaly;
     event.stopPropagation();
     if (this.expandedDay) {
-      this.handleGetSessionsByVeIdRanged(anomaly.date).pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (sessions: Session[]) => {
-          console.log("sessions fetched: ", sessions);
-          let count = 1;
-          sessions.forEach(session => {
-            session.table_id = count++;
-          });
+      this.handleGetSessionsByVeIdRanged(anomaly.date)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (sessions: Session[]) => {
+            console.log('sessions fetched: ', sessions);
+            if (sessions) {
+              let count = 1;
+              sessions.forEach((session) => {
+                session.table_id = count++;
+              });
 
-          this.allSessions = sessions;
+              this.allSessions = sessions;
 
-          this.sessionsTableData.data = sessions.slice(0, 5);
-          this.cd.detectChanges();
-        }
-      });
+              this.sessionsTableData.data = sessions.slice(0, 5);
+            } else {
+              this.allSessions = [];
+              this.sessionsTableData.data = [];
+            }
+            this.cd.detectChanges();
+          },
+        });
     }
   }
 
@@ -205,11 +264,12 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
     const dateTo = new Date(date);
     dateTo.setDate(dateTo.getDate() + 1);
 
-    return this.sessionApiService.getSessionsByVeIdRanged(this.vehicle.veId, date, dateTo)
+    return this.sessionApiService
+      .getSessionsByVeIdRanged(this.vehicle.veId, date, dateTo)
       .pipe(
         takeUntil(this.destroy$),
-        catchError(error => {
-          console.error("Error fetching sessions:", error);
+        catchError((error) => {
+          console.error('Error fetching sessions:', error);
           return of([]); // Return an empty array Observable on error
         })
       );
@@ -220,45 +280,49 @@ export class SessionTableComponent implements OnChanges, AfterViewInit {
    */
   loadMore(button: MatButton): void {
     this.sessionsTableData.data = this.allSessions;
-    button._elementRef.nativeElement.style.display = "none"; // Accessing native element correctly
+    button._elementRef.nativeElement.style.display = 'none'; // Accessing native element correctly
   }
 
-  showPathBySession(session: Session){
-    const points = session.history.map(history => {
+  showPathBySession(session: Session) {
+    const points = session.history.map((history) => {
       return new Point(history.latitude, history.longitude);
     });
     const pathData: any = {
       plate: this.vehicle.plate,
       position_number: session.table_id,
-      points: points
-    }
-    console.log("session pathData: ", pathData);
+      points: points,
+    };
+    console.log('session pathData: ', pathData);
     this.mapService.loadSessionPath$.next(pathData);
   }
 
   showDayPath(anomalyDay: Anomaly) {
-    this.handleGetSessionsByVeIdRanged(anomalyDay.date).subscribe(sessions => {
-      if (sessions.length === 0) {
-        return;
+    this.handleGetSessionsByVeIdRanged(anomalyDay.date).subscribe(
+      (sessions) => {
+        if (sessions.length === 0) {
+          return;
+        }
+
+        const points = sessions.flatMap((session) =>
+          session.history.map(
+            (posizione) => new Point(posizione.latitude, posizione.longitude)
+          )
+        );
+
+        const firstPoints = sessions.map((session) => {
+          const firstHistory = session.history[0];
+          return new Point(firstHistory.latitude, firstHistory.longitude);
+        });
+
+        const pathData = {
+          plate: this.vehicle.plate,
+          points: points,
+          firstPoints: firstPoints,
+        };
+
+        console.log('day pathData: ', pathData);
+        this.mapService.loadDayPath$.next(pathData);
       }
-
-      const points = sessions.flatMap(session =>
-        session.history.map(posizione => new Point(posizione.latitude, posizione.longitude))
-      );
-
-      const firstPoints = sessions.map(session => {
-        const firstHistory = session.history[0];
-        return new Point(firstHistory.latitude, firstHistory.longitude);
-      });
-
-      const pathData = {
-        plate: this.vehicle.plate,
-        points: points,
-        firstPoints: firstPoints
-      };
-
-      console.log("day pathData: ", pathData);
-      this.mapService.loadDayPath$.next(pathData);
-    });
+    );
   }
 }
