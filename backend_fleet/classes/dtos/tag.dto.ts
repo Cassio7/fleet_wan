325f42bf1672d5e052f6e3300af80b99abc85dfc
AlessioC <AlessioC@nomail.com>
2025-03-03 12:59:44 +0000

    less DTO for tag

--- backend_fleet/classes/dtos/tag.dto.ts
+++ backend_fleet/classes/dtos/tag.dto.ts
@@ -1,7 +1,21 @@
-import { IsString } from 'class-validator';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 import { CommonDTO } from 'classes/common/common.dto';
 
 export class TagDTO extends CommonDTO {
   @IsString()
   epc: string;
+  @IsString()
+  tid: string;
+  @IsNumber()
+  detection_quality: number;
+  @IsDate()
+  timestamp: Date;
+  @IsNumber()
+  latitude: number;
+  @IsNumber()
+  longitude: number;
+  @IsNumber()
+  nav_mode: number;
+  @IsString()
+  geozone: string;
 }
