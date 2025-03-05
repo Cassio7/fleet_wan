59d074d7af6fff635ef163fb991fd5356bd087d3
Lorenzo <lorenzo@nomail.com>
2025-03-05 10:58:52 +0000

    indicatore appartenenza mappa

--- frontend_fleet/src/app/Storico-mezzi/Components/storico-mezzi/storico-mezzi.component.ts
+++ frontend_fleet/src/app/Storico-mezzi/Components/storico-mezzi/storico-mezzi.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MapComponent } from "../../../Common-components/map/map.component";
 import { SessionTableComponent } from "../../../Common-components/session-table/session-table.component";
 import { ListaMezziComponent } from "../lista-mezzi/lista-mezzi.component";
@@ -10,12 +10,13 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MapService } from '../../../Common-services/map/map.service';
+import { MapService, pathData } from '../../../Common-services/map/map.service';
 import { skip, Subject, takeUntil, map, filter } from 'rxjs';
 import { RealtimeData } from '../../../Models/RealtimeData';
 import { CommonModule } from '@angular/common';
 import { SessionStorageService } from '../../../Common-services/sessionStorage/session-storage.service';
 import { Point } from '../../../Models/Point';
+import { TagService } from '../../../Common-services/tag/tag.service';
 
 @Component({
   selector: 'app-storico-mezzi',
@@ -37,9 +38,10 @@ import { Point } from '../../../Models/Point';
   templateUrl: './storico-mezzi.component.html',
   styleUrl: './storico-mezzi.component.css'
 })
-export class StoricoMezziComponent implements AfterViewInit{
+export class StoricoMezziComponent implements AfterViewInit, OnDestroy{
   private readonly destroy$: Subject<void> = new Subject<void>();
   private _selectedVehicle!: Vehicle;
+  private _mapPlate!: string;
 
   linedPath: boolean = false;
   isPathLoaded: boolean = false;
@@ -51,16 +53,33 @@ export class StoricoMezziComponent implements AfterViewInit{
     private cd: ChangeDetectorRef
   ){}
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   ngAfterViewInit(): void {
+    this.handlePositionLoading();
     this.handleDayPathLoading();
     this.handleSessionPathLoading();
+  }
+
+  private handlePositionLoading(){
+    this.mapService.loadPosition$.pipe(takeUntil(this.destroy$), skip(1))
+    .subscribe({
+      next: (realtimeData: RealtimeData | null) => {
+        if(realtimeData) this.mapPlate = realtimeData?.vehicle.plate;
+        this.cd.detectChanges();
+      }
+    });
   }
 
   private handleDayPathLoading(){
     this.mapService.loadDayPath$.pipe(takeUntil(this.destroy$), skip(1))
     .subscribe({
-      next: () => {
+      next: (pathData: pathData) => {
         this.isPathLoaded = true;
+        this.mapPlate = pathData.plate;
         this.cd.detectChanges();
       },
     });
@@ -69,8 +88,9 @@ export class StoricoMezziComponent implements AfterViewInit{
   private handleSessionPathLoading(){
     this.mapService.loadSessionPath$.pipe(takeUntil(this.destroy$), skip(1))
     .subscribe({
-      next: () => {
+      next: (pathData: pathData) => {
         this.isPathLoaded = true;
+        this.mapPlate = pathData.plate;
         this.cd.detectChanges();
       },
     });
@@ -91,13 +111,13 @@ export class StoricoMezziComponent implements AfterViewInit{
       this.pathTypeText = "Ricostruito";
     }
 
-    let pathData = JSON.parse(this.sessionStorageService.getItem("pathData"));
+    let pathData: pathData = JSON.parse(this.sessionStorageService.getItem("pathData"));
     const pathType = this.mapService.pathType();
 
     console.log("path type: ", pathType);
 
     if(pathData){
-      //trasformazioni oggetti {_lat, _long} in Point
+      //trasformazione oggetti {_lat, _long} in Point
       pathData.points = pathData.points.map((point: any) => new Point(point._lat, point._long));
       if(pathData?.firstPoints){
         pathData.firstPoints = pathData.firstPoints.map((point: any) => new Point(point._lat, point._long));
@@ -112,6 +132,12 @@ export class StoricoMezziComponent implements AfterViewInit{
 
   }
 
+  public get mapPlate(): string {
+    return this._mapPlate;
+  }
+  public set mapPlate(value: string) {
+    this._mapPlate = value;
+  }
   public get selectedVehicle(): Vehicle {
     return this._selectedVehicle;
   }
