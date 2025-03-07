66b0e749343a372154f4832647b9a74f4bb1b4c5
AlessioC <AlessioC@nomail.com>
2025-03-07 12:32:24 +0000

    createNotification

--- backend_fleet/src/notifications/notifications.service.ts
+++ backend_fleet/src/notifications/notifications.service.ts
@@ -1,12 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { NotificationEntity } from 'classes/entities/notification.entity';
+import { DataSource } from 'typeorm';
 import { NotificationsGateway } from './notifications.gateway';
+import { UserEntity } from 'classes/entities/user.entity';
 
 @Injectable()
 export class NotificationsService {
-  constructor(private readonly notificationsGateway: NotificationsGateway) {}
+  constructor(
+    @InjectDataSource('mainConnection')
+    private readonly connection: DataSource,
+    private readonly notificationsGateway: NotificationsGateway,
+  ) {}
 
   // Metodo per inviare una notifica tramite WebSocket
   sendNotification(message: string) {
     this.notificationsGateway.handleSendNotificationServer(message);
+  }
+
+  /**
+   * Creazione di una notifica ed associo all utente
+   * @param userId user id 
+   * @param message messaggio
+   */
+  async createNotification(userId: number, message: string) {
+    const queryRunner = this.connection.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      const user = await queryRunner.manager.getRepository(UserEntity).findOne({
+        where: {
+          id: userId,
+        },
+      });
+      if (!user) {
+      }
+      const notification = queryRunner.manager
+        .getRepository(NotificationEntity)
+        .create({
+          isRead: false,
+          message: message,
+          user: user,
+        });
+      await queryRunner.manager
+        .getRepository(NotificationEntity)
+        .save(notification);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      if (error instanceof HttpException) throw error;
+      throw new HttpException(
+        `Errore durante la creazione dell'utente`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
