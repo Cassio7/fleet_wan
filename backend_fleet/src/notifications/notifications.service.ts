e89e987d0accfb55056c9eec8df492461122c07e
AlessioC <AlessioC@nomail.com>
2025-03-07 16:22:49 +0000

    updateNotificationRead and getNotifications and toDTO

--- backend_fleet/src/notifications/notifications.service.ts
+++ backend_fleet/src/notifications/notifications.service.ts
@@ -1,9 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { NotificationEntity } from 'classes/entities/notification.entity';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { NotificationsGateway } from './notifications.gateway';
 import { UserEntity } from 'classes/entities/user.entity';
+import { NotificationDto } from 'classes/dtos/notification.dto';
 
 @Injectable()
 export class NotificationsService {
@@ -11,6 +12,8 @@ export class NotificationsService {
     @InjectDataSource('mainConnection')
     private readonly connection: DataSource,
     private readonly notificationsGateway: NotificationsGateway,
+    @InjectRepository(NotificationEntity, 'readOnlyConnection')
+    private readonly notificationRepository: Repository<NotificationEntity>,
   ) {}
 
   // Metodo per inviare una notifica tramite WebSocket
@@ -20,7 +23,7 @@ export class NotificationsService {
 
   /**
    * Creazione di una notifica ed associo all utente
-   * @param userId user id 
+   * @param userId user id
    * @param message messaggio
    */
   async createNotification(userId: number, message: string) {
@@ -34,6 +37,8 @@ export class NotificationsService {
         },
       });
       if (!user) {
+        await queryRunner.rollbackTransaction();
+        return;
       }
       const notification = queryRunner.manager
         .getRepository(NotificationEntity)
@@ -45,6 +50,7 @@ export class NotificationsService {
       await queryRunner.manager
         .getRepository(NotificationEntity)
         .save(notification);
+      await queryRunner.commitTransaction();
     } catch (error) {
       await queryRunner.rollbackTransaction();
       if (error instanceof HttpException) throw error;
@@ -55,5 +61,86 @@ export class NotificationsService {
     } finally {
       await queryRunner.release();
     }
+  }
+
+  /**
+   * Aggiorna lo stato della notifica a letta
+   * @param notificationId
+   * @returns
+   */
+  async updateNotificationRead(
+    notificationId: number,
+  ): Promise<NotificationEntity | null> {
+    const queryRunner = this.connection.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      if (!Number(notificationId)) {
+        await queryRunner.rollbackTransaction();
+        return null;
+      }
+      const notification = await this.notificationRepository.findOne({
+        where: {
+          id: notificationId,
+        },
+      });
+      if (!notification) {
+        await queryRunner.rollbackTransaction();
+        return null;
+      }
+      await queryRunner.manager.getRepository(NotificationEntity).update(
+        {
+          key: notification.key,
+        },
+        { isRead: true },
+      );
+      await queryRunner.commitTransaction();
+      return notification;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      if (error instanceof HttpException) throw error;
+      throw new HttpException(
+        `Errore durante la creazione dell'utente`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  /**
+   * Ritorna tutte le notifiche in base all utente che le richiede
+   * @param userId
+   * @returns
+   */
+  async getNotifications(userId: number): Promise<NotificationDto[] | null> {
+    try {
+      if (!Number(userId)) {
+        return;
+      }
+      const notifications = await this.notificationRepository.find({
+        where: {
+          user: {
+            id: userId,
+          },
+        },
+      });
+      return notifications
+        ? notifications.map((notification) => this.toDTO(notification))
+        : null;
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      throw new HttpException(
+        `Errore durante la creazione dell'utente`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  private toDTO(notification: NotificationEntity): NotificationDto {
+    const notDTO = new NotificationDto();
+    notDTO.message = notification.message;
+    notDTO.isRead = notification.isRead;
+    return notDTO;
   }
 }
