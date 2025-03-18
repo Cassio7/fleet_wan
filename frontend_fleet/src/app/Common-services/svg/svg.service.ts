6ab1812bb4ecccf4eb747ef8e063a35f580acf9e
AlessioC <AlessioC@nomail.com>
2025-03-18 09:04:13 +0000

    service for svg icons

---
+++ frontend_fleet/src/app/Common-services/svg/svg.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SvgService {
+  serviceMap = new Map<number, string>([
+    [1, 'lavaggio.svg'],
+    [2, 'movimentazione.svg'],
+    [3, 'pedana.svg'],
+    [4, 'raccolta.svg'],
+    [5, 'ragno.svg'],
+    [6, 'sanitari.svg'],
+    [7, 'spazzamento.svg'],
+    [8, 'trasferenza.svg'],
+  ]);
+  constructor() {}
+
+  getServiceIcon(serviceId: number): string {
+    const imageName = this.serviceMap.get(serviceId);
+    return imageName
+      ? `assets/service/${imageName}`
+      : 'assets/service/default.svg'; // Default per servizi non trovati
+  }
+}
