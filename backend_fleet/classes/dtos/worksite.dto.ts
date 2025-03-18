42a07375a8c8bb8ace4271c2bf1cfdb6b0775e75
AlessioC <AlessioC@nomail.com>
2025-03-18 11:49:44 +0000

    group and vehicle count

--- backend_fleet/classes/dtos/worksite.dto.ts
+++ backend_fleet/classes/dtos/worksite.dto.ts
@@ -1,7 +1,11 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { CommonDTO } from 'classes/common/common.dto';
+import { GroupDTO } from './group.dto';
 export class WorksiteDTO extends CommonDTO {
   @IsOptional()
   @IsString()
-  name?: string | null;
+  name: string | null;
+  @IsNumber()
+  vehicleCount: number;
+  group: GroupDTO;
 }
