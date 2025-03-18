65a7ec64a45a2fc5735ae311c5f39a1cb1ad44c9
Lorenzo <lorenzo@nomail.com>
2025-03-18 11:12:51 +0000

    all users call on parent home gestione component

--- frontend_fleet/src/app/Gestione-utenti/Components/home-gestione/home-gestione.component.ts
+++ frontend_fleet/src/app/Gestione-utenti/Components/home-gestione/home-gestione.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { UtentiTableComponent } from "../utenti-table/utenti-table.component";
 import { GestioneFiltersComponent } from "../gestione-filters/gestione-filters.component";
+import { AuthService } from '../../../Common-services/auth/auth.service';
+import { User } from '../../../Models/User';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home-gestione',
   standalone: true,
-  imports: [UtentiTableComponent, GestioneFiltersComponent],
+  imports: [CommonModule, UtentiTableComponent, GestioneFiltersComponent],
   templateUrl: './home-gestione.component.html',
   styleUrl: './home-gestione.component.css'
 })
-export class HomeGestioneComponent {
+export class HomeGestioneComponent implements OnInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject<void>();
+  users!: User[];
 
+  constructor(private authService: AuthService, private cd: ChangeDetectorRef){}
+
+  ngOnDestroy(): void {
+   this.destroy$.next()
+   this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.authService.getAllUser().pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (users: User[]) => {
+        this.users = users;
+        console.log('users fetched from home getsione: ', users);
+        this.cd.detectChanges();
+      },
+      error: error => console.error("Errore nel recupero di tutti gli utenti: ",error)
+    });
+  }
 }
--- frontend_fleet/src/app/Gestione-utenti/Components/utenti-table/utenti-table.component.ts
+++ frontend_fleet/src/app/Gestione-utenti/Components/utenti-table/utenti-table.component.ts
@@ -1,12 +1,12 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MatTable, MatTableModule } from '@angular/material/table';
-import { AuthService } from '../../../Common-services/auth/auth.service';
 import { Subject, takeUntil } from 'rxjs';
 import { User } from '../../../Models/User';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { GestioneFilters, GestioneService } from '../../Services/gestione/gestione.service';
 
 @Component({
   selector: 'app-utenti-table',
@@ -25,21 +25,26 @@ export class UtentiTableComponent implements AfterViewInit{
   @ViewChild('utentiTable', {static: false}) utentiTable!: MatTable<User>;
   displayedColumns: string[] = ['Id', 'Utente', 'Username', 'E-mail', 'Ruolo', 'Stato', 'Azioni'];
   utentiTableData: User[] = [];
+  @Input() users!: User[];
 
   constructor(
-    private authService: AuthService,
+    private gestioneService: GestioneService,
     private router: Router
   ){}
 
 
   ngAfterViewInit(): void {
-    this.authService.getAllUser().pipe(takeUntil(this.destroy$))
+    this.utentiTableData = this.users.filter(user => user.role != "Admin");
+    this.utentiTable.renderRows();
+
+    this.gestioneService.filterUsers$.pipe(takeUntil(this.destroy$))
     .subscribe({
-      next: (users: User[]) => {
-        this.utentiTableData = users.filter(user => user.role != "Admin");
-        this.utentiTable.renderRows();
+      next: (gestoneFilters: GestioneFilters | null) => {
+        if(gestoneFilters){
+          this.utentiTableData = this.gestioneService.filterUsersByUsernameResearchAndRoles(this.users, gestoneFilters);
+        }
       },
-      error: error => console.error("Errore nel recupero di tutti gli utenti: ",error)
+      error: error => console.error("Errore nell'applicazione dei filtri sugli utenti: ", error)
     });
   }
 
@@ -48,7 +53,7 @@ export class UtentiTableComponent implements AfterViewInit{
    * @param user utente di cui visionare il profilo da poter modificare
    */
   showProfile(user: User){
-    this.router.navigate(['/profile', user.name, user.id]);
+    this.router.navigate(['/profile', user.id]);
   }
 
   disabilitateUser(user: User){
