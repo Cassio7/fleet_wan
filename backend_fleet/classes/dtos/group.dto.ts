f4d6bb4e5357294cdb88360014d4de25b172dd18
AlessioC <AlessioC@nomail.com>
2025-03-18 11:49:32 +0000

    companyDTO

--- backend_fleet/classes/dtos/group.dto.ts
+++ backend_fleet/classes/dtos/group.dto.ts
@@ -1,9 +1,11 @@
 import { IsNumber, IsString } from 'class-validator';
 import { CommonDTO } from 'classes/common/common.dto';
+import { CompanyDTO } from './company.dto';
 
 export class GroupDTO extends CommonDTO {
   @IsNumber()
   vgId: number;
   @IsString()
   name: string;
+  company: CompanyDTO;
 }
