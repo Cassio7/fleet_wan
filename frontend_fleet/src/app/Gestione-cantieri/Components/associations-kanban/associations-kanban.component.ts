016ac68bbef0147149666ba79726e95b75d581f5
Lorenzo <lorenzo@nomail.com>
2025-04-01 15:20:21 +0000

    associations kanban saving moved vehicle

--- frontend_fleet/src/app/Gestione-cantieri/Components/associations-kanban/associations-kanban.component.ts
+++ frontend_fleet/src/app/Gestione-cantieri/Components/associations-kanban/associations-kanban.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, inject, Input, OnDestroy } from '@angular/core';
 import {
   CdkDrag,
   CdkDragDrop,
@@ -10,21 +10,47 @@ import {
 import { Vehicle } from '../../../Models/Vehicle';
 import { WorkSite } from '../../../Models/Worksite';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { MoveCantiereDialogComponent } from '../move-cantiere-dialog/move-cantiere-dialog.component';
+import { Subject, takeUntil } from 'rxjs';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { SnackbarComponent } from '../../../Common-components/snackbar/snackbar.component';
+import { GestioneCantieriService } from '../../../Common-services/gestione-cantieri/gestione-cantieri.service';
 
 @Component({
   selector: 'app-associations-kanban',
   standalone: true,
-  imports: [CdkDropListGroup, CdkDropList, CdkDrag, MatIconModule],
+  imports: [
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag,
+    MatSnackBarModule,
+    MatIconModule],
   templateUrl: './associations-kanban.component.html',
   styleUrl: './associations-kanban.component.css'
 })
-export class AssociationsKanbanComponent implements AfterViewInit{
+export class AssociationsKanbanComponent implements AfterViewInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject<void>();
+
+  readonly dialog = inject(MatDialog);
+  private snackbar = inject(MatSnackBar);
+
+  @Input() worksite!: WorkSite;
   @Input() worksiteVehicles: Vehicle[] = [];
   @Input() freeVehicles: Vehicle[] = [];
 
   worksiteList: Vehicle[] = [];
 
   freeList: Vehicle[] = [];
+
+  constructor(
+    private gestioneCantieriService: GestioneCantieriService
+  ){}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngAfterViewInit(): void {
     console.log('worksiteVehicles: ', this.worksiteVehicles);
@@ -38,15 +64,77 @@ export class AssociationsKanbanComponent implements AfterViewInit{
   }
 
   drop(event: CdkDragDrop<Vehicle[]>) {
+    const draggedVehicle = event.previousContainer.data[event.previousIndex];
+    const sourceListId = event.previousContainer.id;
+    const destinationListId = event.container.id;
+
+    console.log('sourceListId: ', sourceListId);
+    console.log('destinationListId: ', destinationListId);
+
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
+      const dialogRef = this.dialog.open(MoveCantiereDialogComponent);
+
+      dialogRef.afterClosed()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (result: { dateFrom: string, comment: string }) => {
+          if(result){
+            if (sourceListId === 'cdk-drop-list-0' && destinationListId === 'cdk-drop-list-1') {
+              this.gestioneCantieriService.freeVehicle(draggedVehicle.veId, result.dateFrom, result.comment).pipe(takeUntil(this.destroy$))
+              .subscribe({
+                next: (response: {message: string}) => {
+                  transferArrayItem(
+                    event.previousContainer.data,
+                    event.container.data,
+                    event.previousIndex,
+                    event.currentIndex,
+                  );
+                  this.openSnackbar("Veicolo liberato!")
+                },
+                error: error => {
+                  console.error("Errore nello spostamento del veicolo: ", error)
+                  this.openSnackbar("Errore nello spostamento del veicolo, spostamento annullato.");
+                }
+              });
+            } else if (sourceListId === 'cdk-drop-list-1' && destinationListId === 'cdk-drop-list-0') {
+              this.gestioneCantieriService.moveVehicleInWorksite(draggedVehicle.veId, this.worksite.id, result.dateFrom, result.comment).pipe(takeUntil(this.destroy$))
+              .subscribe({
+                next: (response: {message: string}) => {
+                  transferArrayItem(
+                    event.previousContainer.data,
+                    event.container.data,
+                    event.previousIndex,
+                    event.currentIndex,
+                  );
+                  this.openSnackbar("Veicolo spostato nel cantiere!")
+                },
+                error: error => {
+                  console.error("Errore nello spostamento del veicolo: ", error)
+                  this.openSnackbar("Errore nello spostamento del veicolo, spostamento annullato.");
+                }
+              });
+            }
+          }else{
+            this.openSnackbar("Spostamento del veicolo annullato.");
+          }
+          console.log('worksiteVehicles: ', this.worksiteVehicles);
+          console.log('this.freeVehicles: ', this.freeVehicles);
+        },
+        error: error => console.error("Errore nella chiusura del dialog per lo spostamento del veicolo: ", error)
+      });
     }
+  }
+
+  /**
+   * Apre la snackbar con il contenuto passato
+   * @param content stringa contenuto della snackbar
+   */
+  openSnackbar(content: string): void {
+    this.snackbar.openFromComponent(SnackbarComponent, {
+      duration: 2 * 1000,
+      data: { content: content }
+    });
   }
 }
