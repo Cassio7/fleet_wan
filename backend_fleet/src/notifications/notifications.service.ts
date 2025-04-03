6281c75b0fea551bc0f7cdcd286d8573adf597ba
Lorenzo <lorenzo@nomail.com>
2025-04-03 13:27:39 +0000

    getNotifications() order by

--- backend_fleet/src/notifications/notifications.service.ts
+++ backend_fleet/src/notifications/notifications.service.ts
@@ -132,6 +132,9 @@ export class NotificationsService {
               id: userId,
             },
           },
+          order: {
+            createdAt: 'DESC', 
+          },
         });
         return notifications
           ? notifications.map((notification) => this.toDTO(notification))
@@ -145,7 +148,11 @@ export class NotificationsService {
           },
           isRead: isRead,
         },
+        order: {
+          createdAt: 'DESC', 
+        },
       });
+      
       return notifications
         ? notifications.map((notification) => this.toDTO(notification))
         : null;
