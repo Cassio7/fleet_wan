ee897722f9889d387065e847c65f0112e7cba59b
Lorenzo <lorenzo@nomail.com>
2025-04-04 11:14:00 +0000

    getGoBackTextByUrl()

--- frontend_fleet/src/app/Common-services/navigation/navigation.service.ts
+++ frontend_fleet/src/app/Common-services/navigation/navigation.service.ts
@@ -17,6 +17,30 @@ export class NavigationService {
     });
   }
 
+  getGoBackTextByUrl(previous_url: string): string{
+    switch (previous_url) {
+      case '/dashboard':
+        return 'Torna alla dashboard';
+      case '/home-mezzi':
+        return 'Torna al parco mezzi';
+      case '/storico-mezzi':
+        return 'Torna allo storico mezzi';
+      case '/home-mappa':
+        return 'Torna alla mappa dei mezzi';
+      case '/scarico-letture':
+        return 'Torna allo scarico delle letture';
+      case '/notifications':
+        return "Torna alla visualizzazione delle notifiche";
+      default:
+        if (previous_url?.includes("dettaglio-mezzo")) {
+          const match = previous_url.match(/dettaglio-mezzo\/(\d+)/);
+          const veId = match ? match[1] : "";
+          return `Torna al dettaglio del mezzo ${veId}`;
+        }
+    }
+    return "";
+  }
+
   getPreviousUrl(): string | null {
     return this.previousUrl;
   }
