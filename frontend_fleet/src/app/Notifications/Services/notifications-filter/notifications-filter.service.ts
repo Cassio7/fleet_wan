152859068520c77b2f0110f40b6d84d49a1b0322
Lorenzo <lorenzo@nomail.com>
2025-04-07 08:14:23 +0000

    updateNotificationReadStatus()

--- frontend_fleet/src/app/Notifications/Services/notifications-filter/notifications-filter.service.ts
+++ frontend_fleet/src/app/Notifications/Services/notifications-filter/notifications-filter.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Notifica } from '../../../Models/Notifica';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommonService } from '../../../Common-services/common service/common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsFilterService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private commonService: CommonService,
+    private cookieService: CookieService
+  ) { }
 
+  /**
+   * Filtra delle notifiche in base ad una ricerca sull'username dell'autore di quest'ultime
+   * @param notifiche notifiche da filtrare
+   * @param usernameResearch ricerca per username
+   * @returns array di notifiche filtrato
+   */
   filterNotificationsByUsername(notifiche: Notifica[], usernameResearch: string): Notifica[]{
     if(usernameResearch){
       return notifiche.filter(notifica => {
@@ -16,5 +30,15 @@ export class NotificationsFilterService {
     }else{
       return notifiche;
     }
+  }
+
+  updateNotificationReadStatus(key: string): Observable<{notification: Notifica, message: string}>{
+    const access_token = this.cookieService.get('user');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${access_token}`,
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.patch<{notification: Notifica, message: string}>(`${this.commonService.url}/notifications/${key}`, {}, {headers});
   }
 }
