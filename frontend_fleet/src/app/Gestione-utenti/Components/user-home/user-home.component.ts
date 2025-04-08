e6256c481cfbc040cfc8eede01b9ad922849a2f9
Lorenzo <lorenzo@nomail.com>
2025-04-08 07:12:41 +0000

    user editing page layout edited

--- frontend_fleet/src/app/Gestione-utenti/Components/user-home/user-home.component.ts
+++ frontend_fleet/src/app/Gestione-utenti/Components/user-home/user-home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
 import { User } from '../../../Models/User';
 import { CommonModule } from '@angular/common';
 import { PasswordResetComponent } from "../password-reset/password-reset.component";
@@ -8,6 +8,11 @@ import { AuthService } from '../../../Common-services/auth/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { WorksiteAssociationsKanbanComponent } from "../worksite-associations-kanban/worksite-associations-kanban.component";
+import { AssociationsService, getAssociationsResponse } from '../../Services/associations/associations.service';
+import { Association } from '../../../Models/Association';
+import { WorkSite } from '../../../Models/Worksite';
+import { Company } from '../../../Models/Company';
 
 @Component({
   selector: 'app-user-home',
@@ -17,45 +22,74 @@ import { MatButtonModule } from '@angular/material/button';
     PasswordResetComponent,
     DatiUtenteComponent,
     MatIconModule,
-    MatButtonModule
-],
+    MatButtonModule,
+    WorksiteAssociationsKanbanComponent
+  ],
   templateUrl: './user-home.component.html',
-  styleUrl: './user-home.component.css'
+  styleUrls: ['./user-home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserHomeComponent implements AfterViewInit{
+export class UserHomeComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject<void>();
 
   @Input() user!: User;
+  associationResponse!: getAssociationsResponse;
+
+  freeWorksites: WorkSite[] = [];
 
   header = {
     title: "",
     subtitle: ""
+  };
+
+  constructor(
+    private associationsService: AssociationsService,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cd: ChangeDetectorRef // Inject ChangeDetectorRef
+  ) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
-  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute){}
-
-  ngAfterViewInit(): void {
-    this.route.params.pipe(takeUntil(this.destroy$))
-    .subscribe({
+  ngOnInit(): void {
+    this.route.params.pipe(takeUntil(this.destroy$)).subscribe({
       next: (params) => {
         const userId = params["id"];
-        this.authService.getUserInfoById(userId).pipe(takeUntil(this.destroy$))
-        .subscribe({
+        this.authService.getUserInfoById(userId).pipe(takeUntil(this.destroy$)).subscribe({
           next: (user: User) => {
             this.user = user;
             this.user.id = userId;
 
             this.header.title = `Profilo ${this.user.name} ${this.user.surname}`;
             this.header.subtitle = "pannello modifica";
+
+            this.getAssociationsById(user.id);
           },
-          error: error => console.error("Errore nel recupero delle informazioni dell'utente: ", error)
+          error: (error) => console.error("Errore nel recupero delle informazioni dell'utente: ", error)
         });
       },
-      error: error => console.error("Errore nella ricezione dei parametri dall'url: ", error)
+      error: (error) => console.error("Errore nella ricezione dei parametri dall'url: ", error)
     });
   }
 
-  goBack(){
+  private getAssociationsById(id: number) {
+    this.associationsService.getAssociationsByUserId(id).pipe(takeUntil(this.destroy$)).subscribe({
+      next: (response: getAssociationsResponse) => {
+        console.log('response: ', response);
+        if (response) {
+          this.associationResponse = response;
+          this.cd.detectChanges();  // Trigger change detection manually
+        }
+      },
+      error: (error) => console.error(`Errore nell'ottenimento delle associazioni dell'utente con id ${id}: `, error)
+    });
+  }
+
+  goBack() {
     this.router.navigate(['/gestione-utenti']);
   }
 }
