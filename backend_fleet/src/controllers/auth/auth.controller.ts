650fb3801ff7e5048e9e2141307219dfa9101162
AlessioC <AlessioC@nomail.com>
2025-04-10 15:41:58 +0000

    api for ban tokens

--- backend_fleet/src/controllers/auth/auth.controller.ts
+++ backend_fleet/src/controllers/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Post,
   Req,
@@ -9,13 +10,13 @@ import {
 } from '@nestjs/common';
 import { Role } from 'classes/enum/role.enum';
 import { UserFromToken } from 'classes/interfaces/userToken.interface';
+import { Request, Response } from 'express';
 import { Roles } from 'src/decorators/roles.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RolesGuard } from 'src/guard/roles.guard';
 import { LogContext } from 'src/log/logger.types';
 import { LoggerService } from 'src/log/service/logger.service';
 import { AuthService } from 'src/services/auth/auth.service';
-import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -118,6 +119,264 @@ export class AuthController {
 
       return res.status(error.status || 500).json({
         message: error.message || 'Errore logout utente',
+      });
+    }
+  }
+
+  /**
+   * API per recuperare tutti i token impostati su bannati
+   * @param req user data
+   * @param res
+   * @returns
+   */
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('ban')
+  async getBannedToken(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+  ) {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Ban Token',
+    };
+    try {
+      const tokens = await this.authService.getAllBannedTokens();
+      if (!tokens?.length) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          'Nessun token bannato',
+        );
+        return res.status(204).json();
+      }
+
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Trovati ${tokens.length} token bannati`,
+      );
+
+      res.status(200).json(tokens);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'list',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore nel recupero dei token bannati',
+      });
+    }
+  }
+
+  /**
+   * API per impostare su redis un token utente come bannato
+   * @param req user data
+   * @param body token utente
+   * @param res
+   * @returns
+   */
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Post('ban')
+  async setBannedToken(
+    @Req() req: Request & { user: UserFromToken },
+    @Body() body: { token: string },
+    @Res() res: Response,
+  ): Promise<Response> {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Ban Token',
+    };
+    const token = body.token;
+    try {
+      if (!String(token)) {
+        this.loggerService.logCrudError({
+          error: new Error('Il token deve essere una stringa'),
+          context,
+          operation: 'update',
+        });
+        return res.status(400).json({
+          message: 'Il token deve essere una stringa',
+        });
+      }
+      await this.authService.setBannedToken(token);
+
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Token inserito nei bannati: ${token}`,
+      );
+
+      return res.status(200).json({ message: 'Token bannato' });
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'update',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore nel ban del token',
+      });
+    }
+  }
+
+  /**
+   * API per eliminare un token fornito da quelli bannati
+   * @param req user data
+   * @param body token da sbannare
+   * @param res
+   * @returns
+   */
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Delete('ban')
+  async deleteBannedToken(
+    @Req() req: Request & { user: UserFromToken },
+    @Body() body: { token: string },
+    @Res() res: Response,
+  ): Promise<Response> {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Ban Token',
+    };
+    const token = body.token;
+    try {
+      if (!String(token)) {
+        this.loggerService.logCrudError({
+          error: new Error('Il token deve essere una stringa'),
+          context,
+          operation: 'delete',
+        });
+        return res.status(400).json({
+          message: 'Il token deve essere una stringa',
+        });
+      }
+      const result = await this.authService.deleteBannedToken(token);
+      if (!result) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'delete',
+          `Token non presente nei bannati: ${token}`,
+        );
+        return res
+          .status(200)
+          .json({ message: 'Token non presente nei bannati' });
+      }
+
+      this.loggerService.logCrudSuccess(
+        context,
+        'delete',
+        `Token rimosso dai bannati: ${token}`,
+      );
+      return res.status(200).json({ message: 'Token sbannato' });
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'delete',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore nel rimozione del token dai bannati',
+      });
+    }
+  }
+
+  /**
+   * API per sbannare tutti i token inseriti su quelli bannati
+   * @param req user data
+   * @param res
+   * @returns
+   */
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Delete('ban/all')
+  async deleteAllBannedToken(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+  ): Promise<Response> {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Ban Token',
+    };
+    try {
+      await this.authService.clearAllBannedTokens();
+
+      this.loggerService.logCrudSuccess(
+        context,
+        'delete',
+        `Tutti i token rimossi dai bannati`,
+      );
+      return res
+        .status(200)
+        .json({ message: 'Tutti i token rimossi dai bannati' });
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'delete',
+      });
+
+      return res.status(error.status || 500).json({
+        message:
+          error.message || 'Errore nel rimozione di tutti i token dai bannati',
+      });
+    }
+  }
+
+  /**
+   * API per recuperare gli utenti attualmente loggati, con il rispettivo token
+   * @param req
+   * @param res
+   * @returns
+   */
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('logged')
+  async getLoggedUsers(
+    @Req() req: Request & { user: UserFromToken },
+    @Res() res: Response,
+  ) {
+    const context: LogContext = {
+      userId: req.user.id,
+      username: req.user.username,
+      resource: 'Logged Users',
+    };
+    try {
+      const users = await this.authService.getLoggedUsers();
+      if (!users?.length) {
+        this.loggerService.logCrudSuccess(
+          context,
+          'list',
+          'Nessun utente loggato',
+        );
+        return res.status(204).json();
+      }
+
+      this.loggerService.logCrudSuccess(
+        context,
+        'list',
+        `Trovati ${users.length} utenti loggati`,
+      );
+
+      res.status(200).json(users);
+    } catch (error) {
+      this.loggerService.logCrudError({
+        error,
+        context,
+        operation: 'list',
+      });
+
+      return res.status(error.status || 500).json({
+        message: error.message || 'Errore nel recupero degli utenti loggati',
       });
     }
   }
