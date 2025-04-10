5ee322060b2530a32d4ca8ebbbad64f186e807c6
AlessioC <AlessioC@nomail.com>
2025-04-10 15:43:16 +0000

    associate client to user, no more name events, add new channel for ban

--- backend_fleet/src/notifications/notifications.gateway.ts
+++ backend_fleet/src/notifications/notifications.gateway.ts
@@ -8,25 +8,43 @@ import {
 } from '@nestjs/websockets';
 import { NotificationDto } from 'classes/dtos/notification.dto';
 import { Server, Socket } from 'socket.io';
+import { AuthService } from 'src/services/auth/auth.service';
 
-@WebSocketGateway({
-  namespace: 'events', // Abilita le richieste da qualsiasi origine (puoi limitare ai tuoi domini)
-})
+@WebSocketGateway()
 @Injectable()
 export class NotificationsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(private readonly authService: AuthService) {}
+  //private clients: Map<string, ClientData> = new Map();
+
   @WebSocketServer()
   server: Server;
 
   // Metodo chiamato quando un client si connette
-  handleConnection(client: Socket): void {
-    console.log(`🔗 Client connesso: ${client.id}`);
+  async handleConnection(client: Socket): Promise<void> {
+    const token = client.handshake.query.token as string;
+    if (!token) {
+      client.disconnect();
+      console.log('client disconnesso');
+      return;
+    }
+    const user = await this.authService.validateToken(token);
+    console.log(`🔗 Client connesso: ${client.id} utente: ${user.username}`);
+    await this.authService.setClientIdRedis(user.key, client.id);
   }
 
   // Metodo chiamato quando un client si disconnette
-  handleDisconnect(client: Socket): void {
+  async handleDisconnect(client: Socket): Promise<void> {
+    const token = client.handshake.query.token as string;
+    if (!token) {
+      client.disconnect();
+      console.log('client disconnesso');
+      return;
+    }
+    const user = await this.authService.validateToken(token);
     console.log(`❌ Client disconnesso: ${client.id}`);
+    await this.authService.deleteClientRedis(user.key);
   }
 
   handleSendNotificationServer(
@@ -36,6 +54,16 @@ export class NotificationsGateway
       this.server.emit('notify', notification);
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async sendMessageToUser(userKey: string, message: string): Promise<void> {
+    const clientId = await this.authService.getClientRedis(userKey);
+    const client = this.server.sockets.sockets.get(clientId);
+    if (client) {
+      client.emit('ban', message);
+    } else {
+      console.log(`Client con user.key ${userKey} non trovato`);
     }
   }
 }
