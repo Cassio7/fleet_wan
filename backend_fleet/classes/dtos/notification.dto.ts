7a14e6cc0b7884bf5eb8901b8ba541536073b598
AlessioC <AlessioC@nomail.com>
2025-04-10 15:40:15 +0000

    format

--- backend_fleet/classes/dtos/notification.dto.ts
+++ backend_fleet/classes/dtos/notification.dto.ts
@@ -2,7 +2,6 @@ import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 import { CommonDTO } from 'classes/dtos/common.dto';
 
 export class NotificationDto extends CommonDTO {
-
   @IsBoolean()
   @IsNotEmpty()
   isRead: boolean;
