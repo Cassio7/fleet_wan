f7e8dc82d98b8ae03d803076ffe7529855b0caee
AlessioC <AlessioC@nomail.com>
2025-04-10 15:42:20 +0000

    format

--- backend_fleet/src/controllers/workzone/workzone.controller.ts
+++ backend_fleet/src/controllers/workzone/workzone.controller.ts
@@ -1,55 +1,61 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { WorkzoneEntity } from 'classes/entities/workzone.entity';
-import { UserFromToken } from 'classes/interfaces/userToken.interface';
-import { LogContext } from 'src/log/logger.types';
-import { LoggerService } from 'src/log/service/logger.service';
-import { WorkzoneService } from 'src/services/workzone/workzone.service';
-import { Response } from 'express';
 import { Role } from 'classes/enum/role.enum';
+import { UserFromToken } from 'classes/interfaces/userToken.interface';
+import { Response } from 'express';
 import { Roles } from 'src/decorators/roles.decorator';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { RolesGuard } from 'src/guard/roles.guard';
+import { LogContext } from 'src/log/logger.types';
+import { LoggerService } from 'src/log/service/logger.service';
+import { WorkzoneService } from 'src/services/workzone/workzone.service';
 
 @UseGuards(AuthGuard, RolesGuard)
+@Roles(Role.Admin)
 @Controller('workzone')
 export class WorkzoneController {
-
   constructor(
     private readonly workzoneService: WorkzoneService,
-    private readonly loggerService: LoggerService
-  ){}
+    private readonly loggerService: LoggerService,
+  ) {}
 
-  @Roles(Role.Admin, Role.Responsabile, Role.Capo)
+  /**
+   * API per il recupero delle zone di lavoro da parte di admin
+   * @param req
+   * @param res
+   * @returns
+   */
   @Get()
   async getAllWorkzones(
     @Req() req: Request & { user: UserFromToken },
     @Res() res: Response,
-  ){
+  ) {
     const context: LogContext = {
       userId: req.user.id,
       username: req.user.username,
       resource: 'Workzone (admin)',
     };
-    try{
-      const workzones: WorkzoneEntity[] = await this.workzoneService.getAllWorkzone();
+    try {
+      const workzones: WorkzoneEntity[] =
+        await this.workzoneService.getAllWorkzone();
 
       if (!workzones?.length) {
         this.loggerService.logCrudSuccess(
           context,
           'list',
-          'Nessuna associazione trovata',
+          'Nessuna zona di lavoro trovata',
         );
         return res.status(204).json();
       }
-      
+
       this.loggerService.logCrudSuccess(
         context,
         'list',
-        `Trovate ${workzones.length} associazioni`,
+        `Trovate ${workzones.length} zone di lavoro`,
       );
 
       res.status(200).json(workzones);
-    }catch(error){
+    } catch (error) {
       this.loggerService.logCrudError({
         error,
         context,
@@ -57,7 +63,7 @@ export class WorkzoneController {
       });
 
       return res.status(error.status || 500).json({
-        message: error.message || 'Errore nel recupero delle associazioni',
+        message: error.message || 'Errore nel recupero delle zone di lavoro',
       });
     }
   }
--- backend_fleet/src/services/workzone/workzone.service.ts
+++ backend_fleet/src/services/workzone/workzone.service.ts
@@ -1,28 +1,33 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Get, HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
 import { WorkzoneEntity } from 'classes/entities/workzone.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class WorkzoneService {
-    constructor(
-        @InjectRepository(WorkzoneEntity, 'readOnlyConnection')
-        private readonly workzoneRepository: Repository<WorkzoneEntity>
-    ){}
+  constructor(
+    @InjectRepository(WorkzoneEntity, 'readOnlyConnection')
+    private readonly workzoneRepository: Repository<WorkzoneEntity>,
+  ) {}
 
-    async getAllWorkzone(){
-        try{
-            return await this.workzoneRepository.find({
-                select: {
-                    id: true,
-                    name: true
-                }
-            });
-        }catch(error){
-            throw new HttpException(
-            `Errore durante il recupero delle workzone`,
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-        }
+  /**
+   * Recupera le zone di lavoro dal database
+   * @returns
+   */
+  async getAllWorkzone(): Promise<WorkzoneEntity[]> {
+    try {
+      return await this.workzoneRepository.find({
+        select: {
+          id: true,
+          name: true,
+        },
+      });
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      throw new HttpException(
+        `Errore durante il recupero delle workzone`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 }
