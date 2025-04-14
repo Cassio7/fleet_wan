282f229161416ea1df6982f4a2ed5d9edaa128bc
Lorenzo <lorenzo@nomail.com>
2025-04-14 07:46:39 +0000

    worksiteHistory model fix

--- frontend_fleet/src/app/Models/Worksite-history.ts
+++ frontend_fleet/src/app/Models/Worksite-history.ts
@@ -1,10 +1,12 @@
+import { WorkSite } from "./Worksite";
+
 export class WorksiteHistory {
   constructor(
     public dateFrom: Date,
     public dateTo: Date | null = null,
     public comment: string | null = null,
     public isActive: boolean = true,
-    public worksiteId: number | null = null,
+    public worksite: WorkSite,
     public vehicleId: number
   ) {}
 }
