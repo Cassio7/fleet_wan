ef4f7f3abf8780ee2467bd2f27e446409142007c
AlessioC <AlessioC@nomail.com>
2025-04-30 11:06:05 +0000

    docker build with version backend

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-cd backend_fleet && docker build -t wastetrucker_backend . 
-cd ..
-cd frontend_fleet && docker build -t wastetrucker_frontend .
+set -e
+
+VERSION=${1:-latest}
+
+echo "Building backend version: $VERSION..."
+docker build -t wastetrucker_backend:$VERSION ./backend_fleet
+docker tag wastetrucker_backend:$VERSION wastetrucker_backend:latest
+
+echo "Building frontend latest"
+docker build -t wastetrucker_frontend ./frontend_fleet
+
+echo "Build completato!"
