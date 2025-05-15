492f08e7f72854400d78811c232f0b3c50964d86
AlessioC <AlessioC@nomail.com>
2025-05-15 15:58:49 +0000

    change version frontend

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -2,12 +2,14 @@
 set -e
 
 VERSION=${1:-latest}
+VERSION2=${2:-latest}
 
 echo "Building backend version: $VERSION..."
 docker build -t wastetrucker_backend:$VERSION ./backend_fleet
 docker tag wastetrucker_backend:$VERSION wastetrucker_backend:latest
 
 echo "Building frontend latest"
-docker build -t wastetrucker_frontend ./frontend_fleet
+docker build -t wastetrucker_frontend:$VERSION2 ./frontend_fleet
+docker tag wastetrucker_frontend:$VERSION2 wastetrucker_frontend:latest
 
 echo "Build completato!"
